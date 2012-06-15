3e1a95e7687294f98d1cb0cd79fad308167c6211
Wei Yang <weiyang@linux.vnet.ibm.com>
2012-06-15 22:32:10 +0800

    kernel code test: list_head for double linked list

---
+++ kernel_code_reading/code_test/list.h
@@ -0,0 +1,53 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  list_head.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/15/2012 10:07:09 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#ifndef _LIST_HEAD_
+#define _LIST_HEAD_
+
+/*
+ * Simple doubly linked list implementation.
+ *
+ * Some of the internal functions ("__xxx") are useful when
+ * manipulating whole lists rather than single entries, as
+ * sometimes we already know the next/prev entries and we can
+ * generate better code by using them directly rather than
+ * using the generic single-entry routines.
+ */
+struct list_head {
+	struct list_head *next, *prev;
+};
+
+#define LIST_HEAD_INIT(name) { &(name), &(name) }
+
+#define LIST_HEAD(name) \
+	struct list_head name = LIST_HEAD_INIT(name)
+
+static inline void INIT_LIST_HEAD(struct list_head *list)
+{
+	list->next = list;
+	list->prev = list;
+}
+
+/**
+ * list_empty - tests whether a list is empty
+ * @head: the list to test.
+ */
+static inline int list_empty(const struct list_head *head)
+{
+	return head->next == head;
+}
+#endif // _LIST_HEAD_
---
+++ kernel_code_reading/code_test/list_case.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  list_case.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/15/2012 10:21:41 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include "list.h"
+
+int main()
+{
+	printf("test case for list\n");
+}
