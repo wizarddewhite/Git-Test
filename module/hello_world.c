0791de49927cf2a0a544601cb5e91ba8c8dc7508
Wei Yang <weiyang@linux.vnet.ibm.com>
2012-11-21 15:48:14 +0800

    kernel module example: fix an error in printk format
    newline is "\n" instead of "/n".

--- module/hello_world.c
+++ module/hello_world.c
@@ -21,12 +21,12 @@
 MODULE_LICENSE("Dual BSD/GPL");
 static int hello_init(void)
 {
-        printk(KERN_ALERT "Hello, world/n");
+        printk(KERN_ALERT "Hello, world\n");
         return 0;
 }
 static void hello_exit(void)
 {
-        printk(KERN_ALERT "Goodbye, cruel world/n");
+        printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 module_init(hello_init);
 module_exit(hello_exit);
