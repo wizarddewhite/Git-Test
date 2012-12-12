99a493db45e6902701a557e88067bcb31680d44f
Wei Yang <weiyang@linux.vnet.ibm.com>
2012-12-12 09:51:19 +0800

    php example: examples on string operation
    See more information on http://www.w3schools.com/php/php_string.asp

---
+++ php/string.php
@@ -0,0 +1,33 @@
+<html>
+<body>
+
+
+<?php
+
+$txt1="Hello World";
+echo $txt1;
+?>
+
+<br/>
+
+<?php
+// this is a concatenation operator
+$txt2="What a nice day.";
+echo $txt1 . " " . $txt2;
+
+?>
+
+<br/>
+
+<?php
+echo strlen("Hello World");
+?>
+
+<br/>
+
+<?php
+echo strpos("Hello World", "World");
+?>
+
+</body>
+</html>
