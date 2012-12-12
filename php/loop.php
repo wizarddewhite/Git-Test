594c2980fd091f80ad22a556dff412e502dd54a0
Wei Yang <weiyang@linux.vnet.ibm.com>
2012-12-12 10:31:42 +0800

    php example: for and foreach loop
    for loop is the same as in c program language.
    
    foreach could iterate on an array.
    
    For more information, please view
    http://www.w3schools.com/php/php_looping_for.asp

--- php/loop.php
+++ php/loop.php
@@ -11,6 +11,17 @@ while($i<=5)
 	$i++;
 }
 
+for ($i=6; $i<=10;$i++)
+{
+	echo "The number is " . $i . "<br>";
+}
+
+$x=array("one", "two", "three");
+foreach ($x as $value)
+{
+	echo $value . "<br>";
+}
+
 ?>
 
 </body>
