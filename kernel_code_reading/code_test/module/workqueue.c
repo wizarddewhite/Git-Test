a584fa163ceccc44167a809c3f9ec968ca5a9f74
Wei Yang <weiyang@linux.vnet.ibm.com>
2013-03-02 07:55:52 +0800

    kernel code test: some fix on workqueue example, make code more robust
    1. use container_of() to make the code more reliable
    2. my_wq_function() free the "my_work" instead of the "work"
    3. pass the "work_struct" instead of the "my_work_t" to queue_work() and
       INIT_WORK()

--- kernel_code_reading/code_test/module/workqueue.c
+++ kernel_code_reading/code_test/module/workqueue.c
@@ -21,11 +21,13 @@ my_work_t *work, *work2;
 
 static void my_wq_function( struct work_struct *work)
 {
-	my_work_t *my_work = (my_work_t *)work;
+	my_work_t *my_work;
+
+	my_work = container_of(work, my_work_t, my_work);
 
 	printk( "my_work.x %d\n", my_work->x );
 
-	kfree( (void *)work );
+	kfree(my_work);
 
 	return;
 }
@@ -39,19 +41,19 @@ int workqueue_init( void )
 		/* Queue some work (item 1) */
 		work = (my_work_t *)kmalloc(sizeof(my_work_t), GFP_KERNEL);
 		if (work) {
-			INIT_WORK( (struct work_struct *)work, my_wq_function );
+			INIT_WORK(&work->my_work, my_wq_function );
 			work->x = 1;
 
-			ret = queue_work(my_wq, (struct work_struct *)work);
+			ret = queue_work(my_wq, &work->my_work);
 		}
 
 		/* Queue some additional work (item 2) */
 		work2 = (my_work_t *)kmalloc(sizeof(my_work_t), GFP_KERNEL);
 		if (work2) {
-			INIT_WORK( (struct work_struct *)work2, my_wq_function );
+			INIT_WORK(&work2->my_work, my_wq_function );
 			work2->x = 2;
 
-			ret = queue_work(my_wq, (struct work_struct *)work2);
+			ret = queue_work(my_wq, &work2->my_work);
 		}
 	}
 
