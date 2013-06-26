4b0ad64839dbe62e074a46d2011f1c3c20e9511a
Wei Yang <weiyang@linux.vnet.ibm.com>
2013-06-26 16:52:26 +0800

    userspace code test: elf file test

---
+++ userspace_app/elf_test.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  elf_test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/26/2013 03:51:51 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Wei Yang (weiyang), weiyang.kernel@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+/* 
+ *  readelf -h elf_test
+ *
+ *  readelf -S elf_test
+ *
+ *
+ * */
+#include <stdio.h>
+int global_data = 4;
+int global_data_2;
+
+int main(int argc, char **argv){ 
+	int local_data = 3;
+	printf("Hello Worldn");
+	printf("global_data = %dn", global_data);
+	printf("global_data_2 = %dn", global_data_2);
+	printf("local_data = %dn", local_data);
+	return (0);
+}
