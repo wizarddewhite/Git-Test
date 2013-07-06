28f85993a0dc782344a56a61af345d240e090137
Wei Yang <weiyang@linux.vnet.ibm.com>
2013-07-06 20:25:31 +0800

    userspace code test: retrieve file lenght through stat()

---
+++ userspace_app/file/stat.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  stat.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/06/2013 08:20:34 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Wei Yang (weiyang), weiyang.kernel@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/io.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main()
+{
+	struct stat st;
+
+	if (stat("makefile", &st))
+		return -1;
+
+	printf("the size of makefile is %d\n", st.st_size);
+
+	return 0;
+}
