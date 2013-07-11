90e234e28213d3bf6ca571f27638c84f84918252
Wei Yang <weiyang@linux.vnet.ibm.com>
2013-07-11 10:49:43 +0800

    userspace code test: add newline at the end of printf

--- userspace_app/elf_test.c
+++ userspace_app/elf_test.c
@@ -30,8 +30,8 @@ int global_data_2;
 int main(int argc, char **argv){ 
 	int local_data = 3;
 	printf("Hello Worldn");
-	printf("global_data = %dn", global_data);
-	printf("global_data_2 = %dn", global_data_2);
-	printf("local_data = %dn", local_data);
+	printf("global_data = %d\n", global_data);
+	printf("global_data_2 = %d\n", global_data_2);
+	printf("local_data = %d\n", local_data);
 	return (0);
 }
