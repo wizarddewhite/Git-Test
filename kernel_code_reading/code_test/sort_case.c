0fab8b634468fb2da6d3320b39989945427a1eb9
Wei Yang <weiyang@linux.vnet.ibm.com>
2013-08-26 17:22:51 +0800

    kernel code test: sort_case array of int pointer

--- kernel_code_reading/code_test/sort_case.c
+++ kernel_code_reading/code_test/sort_case.c
@@ -31,7 +31,7 @@ void int_sort()
 {
 	int i;
 
-	int A[] = {10, 3, 32, 0, 89, 28, 37, 47, 6, 5};
+	int A[10] = {10, 3, 32, 0, 89, 28, 37, 47, 6, 5};
 
 	sort(A, 10, 4, int_comp, NULL);
 
@@ -42,8 +42,45 @@ void int_sort()
 	printf("\n");
 }
 
+int int_pointer_comp(const void* a, const void* b)
+{
+	const int** app = a;
+	const int** bpp = b;
+	const int*  ap = *app;
+	const int*  bp = *bpp;
+
+	return *ap - *bp;
+}
+
+void int_pointer_sort()
+{
+	int i;
+
+	int A[10] = {10, 3, 32, 0, 89, 28, 37, 47, 6, 5};
+	int *pA[10];
+
+	for (i=0; i<10; i++)
+		pA[i] = A + i;
+
+	for (i=0; i<10; i++)
+		printf("%d\n", *pA[i]);
+
+	sort(pA, 10, 4, int_pointer_comp, NULL);
+
+	printf("== Result after sorted ==\n");
+	for(i = 0; i < 10; i++)
+		printf("%d ", *pA[i]);
+	printf("\n");
+
+	printf("== The original array A[] ==\n");
+	for(i = 0; i < 10; i++)
+		printf("%d ", A[i]);
+
+	printf("\n");
+}
+
 int main()
 {
-	int_sort();
+	int_pointer_sort();
 	return 0;
 }
