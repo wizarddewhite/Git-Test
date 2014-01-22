33c05b3c9b30a048fe93abb94c4e6c4cf481d592
Wei Yang <weiyang@linux.vnet.ibm.com>
2014-01-22 10:17:14 +0800

    kernel code test: proc file new api
    Currently, proc fs use a new api to create it.
    
    This example has a very simple read/write function for reference.

--- kernel_code_reading/code_test/module/proc_fs.c
+++ kernel_code_reading/code_test/module/proc_fs.c
@@ -81,6 +81,86 @@ static struct file_operations proc_ops = {
 	.release = seq_release,
 };
 
+static char in_data[20] = "This is a test\n";
+
+static ssize_t file_write(struct file *file, const char __user *buffer,
+		size_t count, loff_t *pos)
+{
+
+	/* Get buffer size */
+	if (*pos + count > 20)
+		count = 20 - *pos;
+
+	printk(KERN_ERR "%s: pos is %lld\n", __func__, *pos);
+	printk(KERN_ERR "%s: count is %ld\n", __func__, count);
+
+	/* Write data to buffer */
+	if (copy_from_user(in_data + *pos, buffer, count))
+		return -EFAULT;
+
+	return count;
+}
+
+static void * file_start(struct seq_file *s, loff_t *pos)
+{
+	loff_t n = *pos;
+	printk(KERN_ERR "%s: pos is %lld\n", __func__, *pos);
+
+	if (n < 20)
+		return in_data + n;
+	else
+		return NULL;
+}
+
+static void * file_next(struct seq_file *s, void *v, loff_t *pos)
+{
+	char *tmp = v;
+	loff_t n = *pos;
+	printk(KERN_ERR "%s: pos is %lld\n", __func__, n);
+
+	n++;
+	if (n < 20) {
+		++*pos;
+		return tmp + 1;
+	}
+	else
+		return NULL;
+}
+
+static void file_stop(struct seq_file *s, void *v)
+{
+	seq_printf(s, "\n");
+}
+
+static int file_show(struct seq_file *s, void *v)
+{
+	char *tmp = v;
+
+	seq_printf(s, "%c", *tmp);
+
+	return 0;
+}
+
+static const struct seq_operations file_seq_ops = {
+	.start = file_start,
+	.next  = file_next,
+	.stop  = file_stop,
+	.show  = file_show
+};
+
+static int file_open(struct inode *inode, struct file *file)
+{
+	return seq_open(file, &file_seq_ops);
+}
+
+static const struct file_operations file_ops = {
+	.open    = file_open,
+	.read    = seq_read,
+	.llseek  = seq_lseek,
+	.release = seq_release,
+	.write   = file_write,
+};
+
 static int proc_fs_init(void)
 {
 	struct proc_dir_entry *proc;
@@ -95,7 +175,13 @@ static int proc_fs_init(void)
 	}
 
 	/* Create an entry under root directory */
-	proc = proc_create("rw", 0666, proc_root, &proc_ops);
+	proc = proc_create("r_file", 0666, proc_root, &proc_ops);
+	if (!proc) {
+		printk(KERN_INFO "Failed to create /proc/TEST/rw\n");
+		goto fail;
+	}
+
+	proc = proc_create("rw_file", 0666, proc_root, &file_ops);
 	if (!proc) {
 		printk(KERN_INFO "Failed to create /proc/TEST/rw\n");
 		goto fail;
