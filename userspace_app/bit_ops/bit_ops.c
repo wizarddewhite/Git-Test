1fca730d3969a8d20c1b4dcb9380e3da1d063b8f
Wei Yang <weiyang@linux.vnet.ibm.com>
2014-01-24 13:31:14 +0800

    userspace code: ffs and fls

--- userspace_app/bit_ops/bit_ops.c
+++ userspace_app/bit_ops/bit_ops.c
@@ -43,6 +43,87 @@ void test_is_power2()
 			96, is_power2(96)?" ":" not ");
 }
 
+/*
+ * find last (most-significant) bit set
+ *
+ * Note fls(0) = 0, fls(1) = 1, fls(0x80000000) = 32.
+ */
+int fls(int x)
+{
+	int r = 32;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xffff0000u)) {
+		x <<= 16;
+		r -= 16;
+	}
+	if (!(x & 0xff000000u)) {
+		x <<= 8;
+		r -= 8;
+	}
+	if (!(x & 0xf0000000u)) {
+		x <<= 4;
+		r -= 4;
+	}
+	if (!(x & 0xc0000000u)) {
+		x <<= 2;
+		r -= 2;
+	}
+	if (!(x & 0x80000000u)) {
+		x <<= 1;
+		r -= 1;
+	}
+	return r;
+}
+
+/*
+ * ffs - find first bit set
+ *
+ * Note ffs(0) = 0, ffs(1) = 1, ffs(0x80000000) = 32.
+ */
+int ffs(int x)
+{
+	int r = 1;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xffff)) {
+		x >>= 16;
+		r += 16;
+	}
+	if (!(x & 0xff)) {
+		x >>= 8;
+		r += 8;
+	}
+	if (!(x & 0xf)) {
+		x >>= 4;
+		r += 4;
+	}
+	if (!(x & 3)) {
+		x >>= 2;
+		r += 2;
+	}
+	if (!(x & 1)) {
+		x >>= 1;
+		r += 1;
+	}
+	return r;
+}
+
+void test_ffs_fls()
+{
+	printf("first bit set in %x is %d\n", 0x120, ffs(0x120));
+	printf("first bit set in %x is %d\n", 0x110, ffs(0x110));
+	printf("first bit set in %x is %d\n", 0x100, ffs(0x100));
+
+	printf("\n");
+
+	printf("last bit set in %x is %d\n", 0x020, fls(0x021));
+	printf("last bit set in %x is %d\n", 0x100, fls(0x180));
+	printf("last bit set in %x is %d\n", 0x800, fls(0x840));
+}
+
 int power2_roundup(int x)
 {
     if (x < 0)
@@ -66,6 +147,6 @@ void test_power2_roundup()
 
 int main()
 {
-	test_power2_roundup();
+	test_ffs_fls();
 	return 0;
 }
