e2e4a2a865df58b28954dc42af8a4cbde64fb319
Wei Yang <weiyang@linux.vnet.ibm.com>
2014-04-27 10:43:43 +0800

    userspace code test: udp

---
+++ userspace_app/network/udp/udp_client.c
@@ -0,0 +1,63 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  udp_client.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/27/2014 10:14:22 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Wei Yang (weiyang), weiyang.kernel@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	int sock;
+	struct sockaddr_in server_addr;
+	struct hostent *host;
+	char send_data[1024];
+
+	host= (struct hostent *) gethostbyname((char *)"127.0.0.1");
+
+
+	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+		perror("socket");
+		exit(1);
+	}
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(5000);
+	server_addr.sin_addr = *((struct in_addr *)host->h_addr);
+	bzero(&(server_addr.sin_zero),8);
+
+	while (1) {
+		printf("Type Something (q or Q to quit):");
+		fgets(send_data, sizeof(send_data), stdin);
+	
+		if ((strcasecmp(send_data , "q\n") == 0)) 
+	    		break;
+		else
+			sendto(sock, send_data, strlen(send_data) + 1, 0,
+			       (struct sockaddr *)&server_addr,
+			       sizeof(struct sockaddr));
+	 
+	}
+
+	return 0;
+}
