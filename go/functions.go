d04edfae71258de99ed39e8446e8492d0b228820
Wei Yang <weiyang@linux.vnet.ibm.com>
2014-12-24 11:20:27 +0800

    go/function: add a function on a new type

--- go/functions.go
+++ go/functions.go
@@ -6,6 +6,15 @@ func add(x int, y int) int {
 	return x + y
 }
 
+type World int
+
+func (w *World) get_val() int {
+	return int(*w) + 80
+}
+
 func main() {
+	var w World
+	w = 1
 	fmt.Println(add(1, 3))
+	fmt.Println("The World is ", w.get_val())
 }
