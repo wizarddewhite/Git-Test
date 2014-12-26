af0b7c7d48e9455fbc4a81c21009b0b76d4dd10d
Wei Yang <weiyang@linux.vnet.ibm.com>
2014-12-26 15:25:09 +0800

    go/net: echo server with thread

---
+++ go/net/echoserver.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	service := ":1201"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleClient(conn)
+	}
+
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		if err != nil {
+			return
+		}
+
+		_, err2 := conn.Write(buf[0:n])
+		if err2 != nil {
+			return
+		}
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error: ", err)
+		os.Exit(1)
+	}
+}
