23861b77c76128f23909f9dadc875939c9c30172
Wei Yang <weiyang@linux.vnet.ibm.com>
2015-02-07 22:15:15 +0800

    userspace example: eventfd
    This is an example eventfd for user space communication.
    
    While this could be used for communication between user space and kernel space.

---
+++ userspace_app/eventfd.c
@@ -0,0 +1,73 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  eventfd.c
+ *
+ *    Description:  http://man7.org/linux/man-pages/man2/eventfd.2.html
+ *                  ./eventfd 1 2 4 7 14
+ *
+ *        Version:  1.0
+ *        Created:  02/07/2015 10:07:52 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Wei Yang (weiyang), weiyang.kernel@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#include <sys/eventfd.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>             /* Definition of uint64_t */
+
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+int main(int argc, char *argv[])
+{
+    int efd, j;
+    uint64_t u;
+    ssize_t s;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <num>...\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    efd = eventfd(0, 0);
+    if (efd == -1)
+        handle_error("eventfd");
+
+    switch (fork()) {
+    case 0:
+        for (j = 1; j < argc; j++) {
+            printf("Child writing %s to efd\n", argv[j]);
+            u = strtoull(argv[j], NULL, 0);
+                    /* strtoull() allows various bases */
+            s = write(efd, &u, sizeof(uint64_t));
+            if (s != sizeof(uint64_t))
+                handle_error("write");
+        }
+        printf("Child completed write loop\n");
+
+        exit(EXIT_SUCCESS);
+
+    default:
+        sleep(2);
+
+        printf("Parent about to read\n");
+        s = read(efd, &u, sizeof(uint64_t));
+        if (s != sizeof(uint64_t))
+            handle_error("read");
+        printf("Parent read %llu (0x%llx) from efd\n",
+                (unsigned long long) u, (unsigned long long) u);
+        exit(EXIT_SUCCESS);
+
+    case -1:
+        handle_error("fork");
+    }
+}
+
