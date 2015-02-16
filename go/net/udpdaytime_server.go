529bd3e6c0fffda22e8e6f5c338cf3e47ec29aa3
Wei Yang <weiyang@linux.vnet.ibm.com>
2015-02-16 09:51:33 +0800

    go/net: udp daytime server/client

---
+++ go/net/udpdaytime_server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	service := ":1201"
+	udpAddr, err := net.ResolveUDPAddr("udp4", service)
+	checkError(err)
+
+	conn, err := net.ListenUDP("udp", udpAddr)
+	checkError(err)
+
+	for {
+		handleClient(conn)
+	}
+}
+
+func handleClient(conn *net.UDPConn) {
+	var buf [512]byte
+
+	_, addr, err := conn.ReadFromUDP(buf[0:])
+	if err != nil {
+		return
+	}
+
+	daytime := time.Now().String()
+	conn.WriteToUDP([]byte(daytime), addr)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error: ", err)
+		os.Exit(1)
+	}
+}
