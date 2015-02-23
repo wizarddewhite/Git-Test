6e83deebe17ba94614b96d816d03edab02052f86
Wei Yang <weiyang@linux.vnet.ibm.com>
2015-02-23 15:28:41 +0800

    go/net: a simple file server

---
+++ go/net/file_server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// deliver files from the directory /var/www
+	//fileServer := http.FileServer(http.Dir("/var/www"))
+	fileServer := http.FileServer(http.Dir("."))
+
+	// register the handler and deliver requests to it
+	err := http.ListenAndServe(":8000", fileServer)
+	checkError(err)
+	// That's it!
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
