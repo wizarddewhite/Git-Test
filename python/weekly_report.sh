9b7cfa267165cec7d41a046e8250df630537a1a5
Wei Yang <richard.weiyang@gmail.com>
2016-02-21 10:30:04 +0800

    move python script to kernel branch

--- python/weekly_report.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-# this is a bash script which is used to help send weekly report.
-# This script use the monday.py to output the date range.
-# The detailed report is in report.txt.
-timestamp=`/home/ywywyang/weekly_report/monday.py`
-mail -s "Weekly Report of Richard ($timestamp)" -r "Wizard <wizarddewhite@gmail.com>" address@mail.list.com < /home/report.txt
