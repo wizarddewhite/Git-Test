53fa31fe705413996d1b4146dff98123edf48a68
Wei Yang <richard.weiyang@gmail.com>
2016-09-03 06:01:50 +0800

    kernel makefile: FORCE target
    A rule with no prerequisites or recipe, and the target of the rule is a
    nonexistent file, then make imagines this target to have been updated whenever
    its rule is run. This implies that all targets depending on this one will
    always have their recipe run.
    
    So we can add this to the rule we want to force it to run every time.

---
+++ kernel_makefile/FORCE/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+
+	a = 3, b = 5;
+
+	print(add(a, b));
+	print(sub(a, b));
+
+	return 0;
+}
---
+++ kernel_makefile/FORCE/unforce.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print(int n)
+{
+	printf("%d\n", n);
+}
