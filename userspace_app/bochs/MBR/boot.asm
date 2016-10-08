ad2350c0d9d67a4cb2e4eff6cf23a8cf1eb29f4c
Wei Yang <richard.weiyang@gmail.com>
2016-10-08 21:05:26 +0800

    bochs: make mesage length a variable

--- userspace_app/bochs/MBR/boot.asm
+++ userspace_app/bochs/MBR/boot.asm
@@ -7,12 +7,13 @@ org    07c00h                      ; 告诉编译器程序加载到7c00处
 DispStr:
     mov    ax, BootMessage
     mov    bp, ax                  ; ES:BP = 串地址
-    mov    cx, 16                  ; CX = 串长度
+    mov    cx, msgLen              ; CX = 串长度
     mov    ax, 01301h              ; AH = 13,  AL = 01h
     mov    bx, 000ch               ; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
     mov    dl, 0                   ; 将DL中的ASCII码显示到屏幕,将'\0'送到DL中，并显示
     int    10h                     ; 10h 号中断
     ret                            ; 返回到调用处
 BootMessage:        db    "Hello, OS world!"
+msgLen: equ $ - BootMessage
 times     510-($-$$)    db    0    ; 填充剩下的空间，使生成的二进制代码恰好为512字节
 dw     0xaa55                      ; 结束标志
