779509054187e39170ff19e02a9619df4b2a2dea
Wei Yang <richard.weiyang@gmail.com>
2017-04-27 14:46:30 +0800

    kernel code test: alloc more to see the effect of /proc/slabinfo

--- kernel_code_reading/code_test/module/mm/kmem_cache.c
+++ kernel_code_reading/code_test/module/mm/kmem_cache.c
@@ -21,8 +21,10 @@
 #include <linux/slab.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
+#define ALLOC_NUM 32
+
 static struct kmem_cache *test_cache;
-void *tmp;
+void* tmp[ALLOC_NUM];
 
 static void init_once(void *foo)
 {
@@ -32,19 +34,23 @@ static void init_once(void *foo)
 
 static int kmem_cache_test_init(void)
 {
+	int i;
 	test_cache = kmem_cache_create("test_cache", 1234, 4,
 			0, init_once);
 	if (test_cache == NULL)
 		return -ENOMEM;
 
 	printk(KERN_ERR "%s: Fetch a cache from kmem_cache\n", __func__);
-	tmp = kmem_cache_zalloc(test_cache, GFP_KERNEL);
+	for (i = 0; i < ALLOC_NUM; i++)
+		tmp[i] = kmem_cache_zalloc(test_cache, GFP_KERNEL);
 
 	return 0;
 }
 static void kmem_cache_test_exit(void)
 {
-	kmem_cache_free(test_cache, tmp);
+	int i;
+	for (i = 0; i < ALLOC_NUM; i++)
+		kmem_cache_free(test_cache, tmp[i]);
 	kmem_cache_destroy(test_cache);
 }
 module_init(kmem_cache_test_init);
