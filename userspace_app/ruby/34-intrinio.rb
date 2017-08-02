fec2927f8c91ab34d6e95f44cd4f512f7c6e7f04
Wei Yang <richard.weiyang@gmail.com>
2017-08-02 14:45:57 +0800

    intrinio: get company info

---
+++ userspace_app/ruby/34-intrinio.rb
@@ -0,0 +1,12 @@
+require 'http'
+require 'json'
+
+username = "d2fd14cf52e3bef7c7fc7c6a763f5ba3"
+password = "c17ac2a00ccd3731f407a42ea86978be"
+request_url = "https://api.intrinio.com/companies?page_number=3"
+response = HTTP.basic_auth(:user => username, :pass => password)
+                 .get(request_url)
+                 .body
+
+company = JSON.parse(response)
+puts company
