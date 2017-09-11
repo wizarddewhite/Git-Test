c079dd2c007a53329e0982b09f43991e93f9e969
Wei Yang <richard.weiyang@gmail.com>
2017-09-11 08:42:01 +0800

    js fundamental syntax

---
+++ userspace_app/js/js_fundamental.js
@@ -0,0 +1,86 @@
+/* js fundamental */
+
+/* 1. basic data type */
+
+/* 1.1 integer */
+var age = 18;
+console.log('age is %d', age);
+console.log('age is %d', age + 2);
+
+/* 1.2 string */
+var name = 'xiaoming';
+console.log('name is %s', name);
+console.log('name is %s', name + ' zhang');
+
+/* 1.3 boolean */
+var isAdult = false;
+
+if (age >= 18)
+  isAdult = true;
+else
+  isAdult = false;
+
+console.log('%s %s', name, isAdult?'is Adult':'isn\'t Adult');
+
+/* 1.4 list */
+var myLikeColorsList = ['blue', 'green', 'black', 'white'];
+console.log("myLikeColorsList has %d colors", myLikeColorsList.length);
+console.log("\tFirst color is %s", myLikeColorsList[0]);
+// add one to the end
+myLikeColorsList.push('purple');
+// delete one at the beginning
+myLikeColorsList.splice(0,1);
+for (color in myLikeColorsList)
+  console.log("I like %s", myLikeColorsList[color]);
+
+/* define object */
+var JsLessonInfo = {
+  teacher: name,
+  title: 'JavaScript编程入门',
+  beginTime: '2018/8/8',
+  endTime: '2018/9/4',
+  Student: 417,
+  lessionTarget: '帮助更多小白学习JavaScript',
+  githubUrl: 'https://github.com/xugy0926/getting-started-with-javascript'
+};
+console.log(JsLessonInfo);
+JsLessonInfo.teacher = name + ' zhang';
+console.log("Teacher name: %s", JsLessonInfo.teacher);
+
+/* 2. control flow */
+
+/* 2.1 if */
+
+if (age >= 80)
+  console.log("A god");
+else if (age >= 60)
+  console.log("An elder");
+else if (age >= 40)
+  console.log("A strong");
+else
+  console.log("A child");
+
+/* 2.2 switch */
+switch(age)
+{
+case 0:
+  console.log("new baby");
+  break;
+case 18:
+  console.log("grown up");
+  break;
+default:
+  console.log("living?")
+}
+
+/* 2.3 for */
+
+for (var i = 0; i < myLikeColorsList.length; i++)
+  console.log(myLikeColorsList[i])
+
+/* 2.4 while */
+var i = 0;
+while (i < myLikeColorsList.length) {
+  console.log(myLikeColorsList[i]);
+  i++;
+}
