a1703b110a804cf834bb5271e8a32e4aff3a257b
Wei Yang <richard.weiyang@gmail.com>
2017-09-21 09:44:26 +0800

    ruby/github: retrieve number of contributors

--- userspace_app/ruby/35-github.rb
+++ userspace_app/ruby/35-github.rb
@@ -16,14 +16,37 @@ require 'time'
 #puts response['login']
 
 
-# get my account info
-response = JSON.parse(RestClient.get('https://api.github.com/repos/'+ 'bitcoin/bitcoin' + '/commits',
-                {
-                  :params => {
-                    :access_token => 'Token',
-                    :since => Time.mktime(2017, 9, 15).iso8601
-                  }
-                }
-              ))
+# get my commit info
+#response = JSON.parse(RestClient.get('https://api.github.com/repos/'+ 'bitcoin/bitcoin' + '/commits',
+#                {
+#                  :params => {
+#                    :access_token => 'Token',
+#                    :since => Time.mktime(2017, 9, 15).iso8601
+#                  }
+#                }
+#              ))
 
-puts response[0]["commit"]["committer"]["date"]
+#puts response[0]["commit"]["committer"]["date"]
+
+# get my contributor info
+page = 1
+contributors = 0
+while true do
+  raw_response = RestClient.get('https://api.github.com/repos/'+ 'bitcoin/bitcoin' + '/contributors',
+                  {
+                    :params => {
+                      :access_token => 'token',
+                      :page => page
+                    }
+                  }
+                )
+  response = JSON.parse(raw_response)
+  puts "Page " + page.to_s() + " Number " + response.length.to_s()
+  contributors += response.length
+  if response.length == 0
+    break
+  end
+  page += 1
+end
+
+puts contributors
