25d574b1a042e97080274457532791cc14b73aef
Wei Yang <richard.weiyang@gmail.com>
2017-10-01 21:53:29 +0800

    go: adjust the slice.go a bit

--- userspace_app/go/tour-go/slice.go
+++ userspace_app/go/tour-go/slice.go
@@ -2,46 +2,67 @@ package main
 
 import "fmt"
 
-func make_slice() {
-	a := make([]int, 5)
+func create_slice() {
+	/* Format 1 */
+	var p = []int{2, 3, 5, 7, 11, 13}
+	fmt.Println("p ==", p)
+	for i := 0; i < len(p); i++ {
+		fmt.Printf("p[%d] == %d\n", i, p[i])
+	}
+	printSlice("p", p)
+
+	/* Format 2 */
+	var a []int
 	printSlice("a", a)
+	if a == nil {
+		fmt.Println("nil!")
+	}
+	a = make([]int, 5, 5)
+	printSlice("a", a)
+	a[0] = 1
+
+	/* Format 3 */
 	b := make([]int, 0, 5)
 	printSlice("b", b)
+}
+
+func make_slice() {
+	b := make([]int, 0, 5)
+	printSlice("b", b)
+
+	/* Slice of the first two */
 	c := b[:2]
 	printSlice("c", c)
+	fmt.Println(&c)
+
+	/* Slice of the last three */
 	d := c[2:5]
 	printSlice("d", d)
+	fmt.Println(&d)
 }
 
 func append_slice() {
 	var a []int
 	printSlice("a", a)
 
+	/* Append one element */
 	a = append(a, 0)
 	printSlice("a", a)
 
-	a = append(a, 1)
+	/* Append several elements */
+	a = append(a, 1, 2, 3, 4)
 	printSlice("a", a)
 
-	a = append(a, 2, 3, 4)
-	printSlice("a", a)
+	/* Copy a to b */
+	b := make([]int, len(a), cap(a))
+	copy(b, a)
+	printSlice("b", b)
 }
 
 func main() {
-	var p = [...]int{2, 3, 5, 7, 11, 13}
-	fmt.Println("p ==", p)
-	for i := 0; i < len(p); i++ {
-		fmt.Printf("p[%d] == %d\n", i, p[i])
-	}
-
-	var a []int
-	printSlice("a", a)
-	if a == nil {
-		fmt.Println("nil!")
-	}
-
+	//create_slice()
 	make_slice()
-	append_slice()
+	//append_slice()
 }
 
 func printSlice(s string, x []int) {
