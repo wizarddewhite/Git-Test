6be84a8cae82e5f12b68c4279e1b3ae1bf753907
Wei Yang <richard.weiyang@gmail.com>
2017-10-01 22:10:04 +0800

    go: maps operation

--- userspace_app/go/tour-go/maps.go
+++ userspace_app/go/tour-go/maps.go
@@ -8,12 +8,31 @@ type Vertex struct {
 	Lat, Long float64
 }
 
-var m map[string]Vertex
-
 func main() {
+	var m map[string]Vertex
 	m = make(map[string]Vertex)
 	m["Bell Labs"] = Vertex{
 		40.68433, -74.39967,
 	}
+	m["AT&T Labs"] = Vertex{
+		40.68433, -74.39967,
+	}
 	fmt.Println(m["Bell Labs"])
+
+	v, ok := m["AT&T Labs"]
+	if ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("No such element")
+	}
+
+	delete(m, "AT&T Labs")
+
+	v, ok = m["AT&T Labs"]
+	if ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("No such element")
+	}
+
 }
