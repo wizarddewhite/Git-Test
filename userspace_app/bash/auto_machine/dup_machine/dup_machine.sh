70c11d8307aec313d9fa3b837c3589ef432ffeb3
Wei Yang <richard.weiyang@gmail.com>
2018-01-02 14:38:30 +0800

    raw config

--- userspace_app/bash/auto_machine/dup_machine/dup_machine.sh
+++ userspace_app/bash/auto_machine/dup_machine/dup_machine.sh
@@ -5,8 +5,8 @@ if [ $# != 4 ]; then
 	exit -1
 fi
 
-/root/dup_machine/save_config.sh
-echo userinfo updated
+#/root/dup_machine/save_config.sh
+#echo userinfo updated
 
 # setup remote key and copy it to remote
 if [ ! -e /root/.ssh/id_rsa ]; then
@@ -20,29 +20,41 @@ sshpass -p "$2" scp /root/.ssh/id_rsa.pub root@$1:/root/.ssh/authorized_keys
 ssh -o "StrictHostKeyChecking no" root@$1 mkdir -p /root/dup_machine
 
 # copy users.info to remote 
-scp /root/dup_machine/users.info root@$1:/root/dup_machine/
+#scp /root/dup_machine/users.info root@$1:/root/dup_machine/
 
 # copy bandwith monitor to remote
 scp -r /root/calculate_bandwidth root@$1:/root/
+
+# copy other server config
+mkdir -p /root/server
+cp /etc/nginx/nginx.conf /root/server/
+cp /etc/nginx/sites-available/default /root/server/nginx-default
+cp /etc/systemd/system/v2ray.service /root/server/
+cp /usr/bin/v2ray/v2ray /root/server/
+cp /usr/bin/v2ray/v2ctl /root/server/
+cp /etc/v2ray/config.json /root/server/
+cp /etc/v2ray/v2ray.crt /root/server/
+cp /etc/v2ray/v2ray.key /root/server/
+scp -r /root/server root@$1:/root/
 
 # copy restore file to remote
 scp /root/dup_machine/restore_config.sh root@$1:/root/dup_machine/
 
 # copy ssh server
-ssh -o "StrictHostKeyChecking no" root@$1 mkdir -p /usr/local/obfus/sbin
-ssh -o "StrictHostKeyChecking no" root@$1 mkdir -p /usr/local/obfus/etc
-scp /root/sshd/sshd root@$1:/usr/local/obfus/sbin/
-scp /root/sshd/etc/* root@$1:/usr/local/obfus/etc/
+#ssh -o "StrictHostKeyChecking no" root@$1 mkdir -p /usr/local/obfus/sbin
+#ssh -o "StrictHostKeyChecking no" root@$1 mkdir -p /usr/local/obfus/etc
+#scp /root/sshd/sshd root@$1:/usr/local/obfus/sbin/
+#scp /root/sshd/etc/* root@$1:/usr/local/obfus/etc/
 
 # copy keys
-users=`ls /home/`
-for u in $users
-do
-	cp /home/$u/.ssh/authorized_keys /root/keys/$u.key
-done
-tar -zcvf /root/keys.tar.gz /root/keys/
-scp /root/keys.tar.gz root@$1:
-rm -f /root/keys.tar.gz /root/keys/*
+#users=`ls /home/`
+#for u in $users
+#do
+#	cp /home/$u/.ssh/authorized_keys /root/keys/$u.key
+#done
+#tar -zcvf /root/keys.tar.gz /root/keys/
+#scp /root/keys.tar.gz root@$1:
+#rm -f /root/keys.tar.gz /root/keys/*
 
 # execute
 echo do the stuff on remote
--- userspace_app/bash/auto_machine/dup_machine/restore_config.sh
+++ userspace_app/bash/auto_machine/dup_machine/restore_config.sh
@@ -4,44 +4,68 @@
 echo "Port 26" >> /etc/ssh/sshd_config
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 echo "AllowUsers root" >> /etc/ssh/sshd_config
-echo "ObfuscateKeyword $1" >> /usr/local/obfus/etc/sshd_config
-echo "ObfuscatedPort $2" >> /usr/local/obfus/etc/sshd_config
-service sshd restart
-mkdir /var/empty
-/usr/local/obfus/sbin/sshd -f /usr/local/obfus/etc/sshd_config &
+#echo "ObfuscateKeyword $1" >> /usr/local/obfus/etc/sshd_config
+#echo "ObfuscatedPort $2" >> /usr/local/obfus/etc/sshd_config
 usermod -g root -G ssh root
+service sshd restart
+#mkdir /var/empty
+#/usr/local/obfus/sbin/sshd -f /usr/local/obfus/etc/sshd_config &
 
 # install nethogs
-apt-get -y install nethogs
+#apt-get -y install nethogs
 
 # enable tcp brr
 echo 'net.core.default_qdisc=fq' | tee -a /etc/sysctl.conf
 echo 'net.ipv4.tcp_congestion_control=bbr' | tee -a /etc/sysctl.conf
 sysctl -p
 
+# setup server
+cp /root/server/v2ray.service /etc/systemd/system/v2ray.service
+cp /root/server/v2ray.service /etc/systemd/system/multi-user.target.wants/v2ray.service
+mkdir -p /usr/bin/v2ray
+cp /root/server/v2ray /usr/bin/v2ray/v2ray
+cp /root/server/v2ctl /usr/bin/v2ray/v2ctl
+mkdir -p /etc/v2ray
+cp /root/server/config.json /etc/v2ray/config.json
+cp /root/server/v2ray.crt /etc/v2ray/v2ray.crt
+cp /root/server/v2ray.key /etc/v2ray/v2ray.key
+mkdir -p /var/log/v2ray
+
+# install and setup nginx
+apt-get -y install nginx
+cp /root/server/nginx.conf /etc/nginx/nginx.conf
+cp /root/server/nginx-default /etc/nginx/sites-available/default
+service nginx restart
+
+# start server
+service v2ray start
+
 # restore users
 # unpack key file
-tar -xzvf keys.tar.gz -C /
-while read user
-do
-	IFS=' ' read -r -a array <<< "$user"
-	# create user
-	useradd -s /bin/true -d /home/${array[0]} -m ${array[0]} &> /dev/null
-	usermod -p '*' ${array[0]}
-	# create .ssh dir
-	mkdir -p /home/${array[0]}/.ssh
-	# get key file
-	mv /root/keys/${array[0]}.key /home/${array[0]}/.ssh/authorized_keys
-	# chown
-	chown -R ${array[0]}:${array[0]} /home/${array[0]}/.ssh
-	# add to ssh if enabled
-	if [ ${array[1]} -ne 0 ]; then
-		usermod -g ${array[0]} -G ssh ${array[0]}
-	fi
-done < /root/dup_machine/users.info
+#tar -xzvf keys.tar.gz -C /
+#while read user
+#do
+#	IFS=' ' read -r -a array <<< "$user"
+#	# create user
+#	useradd -s /bin/true -d /home/${array[0]} -m ${array[0]} &> /dev/null
+#	usermod -p '*' ${array[0]}
+#	# create .ssh dir
+#	mkdir -p /home/${array[0]}/.ssh
+#	# get key file
+#	mv /root/keys/${array[0]}.key /home/${array[0]}/.ssh/authorized_keys
+#	# chown
+#	chown -R ${array[0]}:${array[0]} /home/${array[0]}/.ssh
+#	# add to ssh if enabled
+#	if [ ${array[1]} -ne 0 ]; then
+#		usermod -g ${array[0]} -G ssh ${array[0]}
+#	fi
+#done < /root/dup_machine/users.info
 
 # deploy bandwidth calculation
-echo " */10    *  * * *   root    cd /root/calculate_bandwidth && ./get_user_bandwidth.sh " >> /etc/crontab
+#echo " */10    *  * * *   root    cd /root/calculate_bandwidth && ./get_user_bandwidth.sh " >> /etc/crontab
+cp /root/calculate_bandwidth/v2ray_bandwidth.service /etc/systemd/system/multi-user.target.wants/v2ray_bandwidth.service
+cp /root/calculate_bandwidth/v2ray_bandwidth.service /etc/systemd/system/v2ray_bandwidth.service
+service v2ray_bandwidth start
 
 # mark done
 echo " -Z $1 -p $2" > /root/done
