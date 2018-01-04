70454d0d7851b05ca419eebe6c71d79f7a89a3e5
Wei Yang <richard.weiyang@gmail.com>
2018-01-05 00:18:04 +0800

    iptable cleanup

--- userspace_app/bash/auto_machine/dup_machine/restore_config.sh
+++ userspace_app/bash/auto_machine/dup_machine/restore_config.sh
@@ -7,6 +7,22 @@ echo "AllowUsers root" >> /etc/ssh/sshd_config
 usermod -g root -G ssh root
 service sshd restart
 
+# iptable clean up
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
+
+ip6tables -P INPUT ACCEPT
+ip6tables -P FORWARD ACCEPT
+ip6tables -P OUTPUT ACCEPT
+ip6tables -t nat -F
+ip6tables -t mangle -F
+ip6tables -F
+ip6tables -X
 
 # enable tcp brr
 echo 'net.core.default_qdisc=fq' | tee -a /etc/sysctl.conf
