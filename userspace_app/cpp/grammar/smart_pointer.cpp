ebf29dca2ae13c3523e7befbb72b9e6606dadf2f
Wei Yang <richard.weiyang@gmail.com>
2018-05-23 09:19:11 +0800

    cpp: smart pointer

---
+++ userspace_app/cpp/grammar/smart_pointer.cpp
@@ -0,0 +1,77 @@
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+struct Base
+{
+    Base() { cout << "Base::Base()" << endl; }
+    Base(string n):name(n) { cout << "Base::Base(" << name << ")" << endl; }
+    ~Base() { cout << "Base::~Base(" << name << ")" << endl; }
+    virtual void print() {cout << "  Base::print(" << name << ")" << endl;}
+
+    string name;
+};
+
+void raw_pointer()
+{
+	Base *d = new Base("raw pointer");
+	d->print();
+
+	// must release the memory by delete
+	delete d;
+}
+
+// deprecated
+void auto_pointer()
+{
+	auto_ptr<Base> sd(new Base("auto pointer"));
+	// automatically released
+
+	auto_ptr<Base> csd;
+	// ownership transferred
+	csd = sd;
+	//sd->print(); // will crash
+	csd->print();
+	cout << " sd addr:" << sd.get() << endl;
+	cout << "csd addr:" << csd.get() << endl;
+}
+
+void shared_pointer()
+{
+	shared_ptr<Base> sd(new Base("shared pointer1"));
+	// automatically released
+
+	shared_ptr<Base> csd;
+	csd = sd;
+	sd->print();
+	csd->print();
+	cout << " sd addr:" << sd.get() << endl;
+	cout << "csd addr:" << csd.get() << endl;
+}
+
+void unique_pointer()
+{
+	unique_ptr<Base> sd(new Base("unique pointer"));
+	// automatically released
+
+	unique_ptr<Base> csd;
+	// not allowed
+	//csd = sd;
+
+	// ownership transferred
+	csd = move(sd);
+	//sd->print(); // will crash
+	csd->print();
+	cout << " sd addr:" << sd.get() << endl;
+	cout << "csd addr:" << csd.get() << endl;
+}
+
+int main()
+{
+	//raw_pointer();
+	auto_pointer();
+	//shared_pointer();
+	//unique_pointer();
+	return 0;
+}
