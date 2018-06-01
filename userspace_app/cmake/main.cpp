1b105257a4be1966a203d627ad57ed3b901951b0
Wei Yang <richard.weiyang@gmail.com>
2018-06-01 23:05:46 +0800

    cmake subdir example

--- userspace_app/cmake/main.cpp
+++ userspace_app/cmake/main.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
+#include <iostream>
+#include "src/Test.h"
 
 int main()
 {
 	std::cout<<"Hello word!"<<std::endl;
+	std::cout<<add(1,2)<<std::endl;
 	return 0;
 }
---
+++ userspace_app/cmake/src/Test.h
@@ -0,0 +1,6 @@
+#ifndef _TEST_
+#define _TEST_
+
+int add(int, int);
+
+#endif //_TEST_
