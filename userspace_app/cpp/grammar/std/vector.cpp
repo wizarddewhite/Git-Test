d7ac27e6fce388f28d2faca3e1df7c02c7c68db5
Wei Yang <richard.weiyang@gmail.com>
2018-07-06 11:09:34 +0800

    std::resize()

--- userspace_app/cpp/grammar/std/vector.cpp
+++ userspace_app/cpp/grammar/std/vector.cpp
@@ -10,17 +10,30 @@ int main(int argc,char **argv)
 	it++;
 	cout<<"now vi is :";
 	for_each(vi.begin(),vi.end(),[](int m){cout<<m<<" ";});
+	cout << endl;
+
 	// it points to 2nd place
 	vi.emplace(it,999);
 	cout<<endl<<"after emplace(it,999)"<<endl;
 	cout<<"now vi is :";
 	for_each(vi.begin(),vi.end(),[](int m){cout<<m<<" ";});
-
 	// emplace_back
 	vi.emplace_back(888);
 	cout<<endl<<"after emplace_back(888)"<<endl;
 	cout<<"now vi is :";
 	for_each(vi.begin(),vi.end(),[](int m){cout<<m<<" ";});
+	cout << endl;
+
+	// resize
+	vi.resize(10);
+	cout<<endl<<"after resize(10)"<<endl;
+	cout<<"now vi is :";
+	for_each(vi.begin(),vi.end(),[](int m){cout<<m<<" ";});
+	vi.resize(3);
+	cout<<endl<<"after resize(3)"<<endl;
+	cout<<"now vi is :";
+	for_each(vi.begin(),vi.end(),[](int m){cout<<m<<" ";});
+	cout << endl;
 	
 	cout<<endl;	
 }
