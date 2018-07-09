4a8d028f868c0b6eecefd02b2577527c0edfbc47
Wei Yang <richard.weiyang@gmail.com>
2018-07-09 16:07:31 +0800

    default member value

--- userspace_app/cpp/grammar/default_parameter.cpp
+++ userspace_app/cpp/grammar/default_parameter.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+struct A {
+	A(int _a):a(_a){}
+	int a = 18;
+};
+
+struct B {
+	A a = A(3);
+	int b = 9;
+};
+
 void Func(int i = 1, float f = 2.0f, double d = 3.0)
 {
     cout << i << ", " << f << ", " << d << endl ;
@@ -11,5 +21,9 @@ int main()
 {
 	Func();
 	Func(10);
+
+	B b;
+	cout << b.a.a << endl;
+	cout << b.b << endl;
 	return 0;
 }
