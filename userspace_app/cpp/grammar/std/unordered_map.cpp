f8690aa01a08ae1fdbba66532e92b5de50994efa
Wei Yang <richard.weiyang@gmail.com>
2018-07-13 09:25:26 +0800

    unordered_map with std type

--- userspace_app/cpp/grammar/std/unordered_map.cpp
+++ userspace_app/cpp/grammar/std/unordered_map.cpp
@@ -2,20 +2,57 @@
 #include <string>
 #include <unordered_map>
 
-int main ()
+using namespace std;
+
+void Print(unordered_map<string, double>& m, string pre)
 {
-  std::unordered_map<std::string,double> mymap = {
+    unordered_map<string, double>::iterator iter;
+    cout << pre << endl;;
+    for (iter = m.begin(); iter != m.end(); ++iter)
+        cout << "(" << iter->first << ", " << iter->second << ")" << endl;
+    cout << endl;
+}
+
+int std_type_unordered_map()
+{
+  unordered_map<string,double> mymap = {
      {"Burger",2.99},
+     {"Burger",3.99},
      {"Fries",1.99},
      {"Soda",1.50},
   };
 
   for (auto& x: {"Burger","Pizza","Salad","Soda"}) {
     if (mymap.count(x)>0)
-      std::cout << "mymap has " << x << std::endl;
+      cout << "mymap has " << mymap.count(x) << " " << x << endl;
     else
-      std::cout << "mymap has no " << x << std::endl;
+      cout << "mymap has no " << x << endl;
+  }
+
+  mymap["Orange"] = 5.05;
+  Print(mymap, "Insert Orange:");
+
+  mymap.insert(mymap.end(), pair<string, double>("Cake", 3.88));
+  Print(mymap, "Insert Cake at end:");
+
+  mymap.emplace("Onion", 2.38);
+  Print(mymap, "Emplace Onion:");
+
+  mymap.at("Onion") = 3.38;
+  Print(mymap, "At Onion:");
+
+  unordered_map<string, double>::iterator iter;
+  iter = mymap.find("Cake");
+  if (iter != mymap.end()) {
+     mymap.erase(iter);
+     Print(mymap, "Erase Cake:");
   }
 
   return 0;
+}
+
+int main()
+{
+   std_type_unordered_map();
+   return 0;
 }
