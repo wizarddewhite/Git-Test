8eed8e602d68d38398801e026d4cf3b95e981e8c
Wei Yang <richard.weiyang@gmail.com>
2018-10-28 10:03:27 +0800

    find last

--- userspace_app/data_struct/16_binary_search_variant.c
+++ userspace_app/data_struct/16_binary_search_variant.c
@@ -56,6 +56,25 @@ int binary_search_first(int *arr, int size, int val)
 		return -1;
 }
 
+int binary_search_last(int *arr, int size, int val)
+{
+	int low = 0, high = size - 1, mid;
+
+	while (low <= high) {
+		mid = (low + high) / 2;
+
+		if (arr[mid] <= val)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+
+	if (arr[high] == val)
+		return high;
+	else
+		return -1;
+}
+
 int binary_search_first_r(int *arr, int size, int val)
 {
 	int mid = size / 2;
@@ -86,6 +105,38 @@ int binary_search_first_r(int *arr, int size, int val)
 	return idx;
 }
 
+int binary_search_last_r(int *arr, int size, int val)
+{
+	int mid = size / 2;
+	int idx;
+
+	if (size <= 0)
+		return -1;
+
+	// we find *val* at mid, try last half
+	if (arr[mid] == val) {
+		idx = binary_search_last_r(arr+mid+1, size-mid-1, val);
+		if (idx != -1)
+			mid += idx + 1;
+		return mid;
+	}
+
+	// mid == 0 means size == 1
+	// so the only element in array doesn't equal to val
+	if (!mid)
+		return -1;
+
+	if (arr[mid] < val) {
+		idx = binary_search_last_r(arr + mid + 1, size - mid - 1, val);
+		if (idx != -1)
+			idx += mid + 1;
+	} else {
+		idx = binary_search_last_r(arr, mid, val);
+	}
+
+	return idx;
+}
+
 int main()
 {
 	int arr[10] = {1, 4, 5, 9, 12, 14, 19, 19, 31, 36};
@@ -100,6 +151,18 @@ int main()
 	idx = binary_search_first_r(arr, 10, 19);
 	if (idx != -1)
 		printf("first 19 at %d\n", idx);
+	else
+		printf("19 not in arr \n");
+
+	idx = binary_search_last(arr, 10, 19);
+	if (idx != -1)
+		printf("last 19 at %d\n", idx);
+	else
+		printf("19 not in arr \n");
+
+	idx = binary_search_last_r(arr, 10, 19);
+	if (idx != -1)
+		printf("last 19 at %d\n", idx);
 	else
 		printf("19 not in arr \n");
 
