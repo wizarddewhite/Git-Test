83264a63c6fce89560b41589a54ae092445333dc
Wei Yang <richard.weiyang@gmail.com>
2019-03-24 23:01:34 +0800

    python: try in-reply-to to get thread

--- userspace_app/python/mail/get_mail.py
+++ userspace_app/python/mail/get_mail.py
@@ -7,11 +7,18 @@ from dateutil.parser import parse
 threads = []
 # a dictionary of {message-id, thread}
 message_ids = {}
+# a dictionary of {in-reply-id, thread}
+in_reply_ids = {}
 
 def put_to_thread(message):
     if message['in-reply-to'] in message_ids:
         # is it reply to some message?
         thread = message_ids[message['in-reply-to']]
+        # add the message to thread
+        thread.add(message)
+    elif message['in-reply-to'] and message['in-reply-to'] in in_reply_ids:
+        # is it reply to the same message as others?
+        thread = in_reply_ids[message['in-reply-to']]
         # add the message to thread
         thread.add(message)
     else:
@@ -20,13 +27,14 @@ def put_to_thread(message):
         threads.append(thread)
 
     # add message-id to dictionary
-    message_ids[message['Message-Id']] = thread
+    in_reply_ids[message['in-reply-to']] = thread
+    message_ids[message['message-id']] = thread
 
 def threadify(mbox_name):
     mbox = mailbox.mbox(mbox_name)
     mbox.lock()
     #for message in mbox:
-    for message_id, message in mbox.iteritems():
+    for idx, message in mbox.iteritems():
         put_to_thread(message)
     mbox.flush()
     mbox.close()
@@ -50,5 +58,3 @@ def iterate_mailbox():
 if __name__ == "__main__":
     #iterate_mailbox()
     threadify('example.mbox')
-
-
