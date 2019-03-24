502946074afd2eb88f04b6b6726327f9fca36ea9
Wei Yang <richard.weiyang@gmail.com>
2019-03-24 22:15:43 +0800

    python: sort mail into thread

--- userspace_app/python/mail/get_mail.py
+++ userspace_app/python/mail/get_mail.py
@@ -1,5 +1,41 @@
 #!/usr/bin/python
+import datetime
 import mailbox
+from dateutil.parser import parse
+
+# a set of threads, each thread is a set of its messages
+threads = []
+# a dictionary of {message-id, thread}
+message_ids = {}
+
+def put_to_thread(message):
+    if message['in-reply-to'] in message_ids:
+        # is it reply to some message?
+        thread = message_ids[message['in-reply-to']]
+        # add the message to thread
+        thread.add(message)
+    else:
+        thread = {message}
+        #add thread to threads
+        threads.append(thread)
+
+    # add message-id to dictionary
+    message_ids[message['Message-Id']] = thread
+
+def threadify(mbox_name):
+    mbox = mailbox.mbox(mbox_name)
+    mbox.lock()
+    #for message in mbox:
+    for message_id, message in mbox.iteritems():
+        put_to_thread(message)
+    mbox.flush()
+    mbox.close()
+
+    for t in threads:
+        print "=====Thread: %d msgs", len(t)
+        for msg in t:
+            datetime = parse(msg['date'])
+            print datetime.date(), msg['from'], msg['subject']
 
 def iterate_mailbox():
     mbox = mailbox.mbox('example.mbox')
@@ -12,4 +48,7 @@ def iterate_mailbox():
     mbox.close()
 
 if __name__ == "__main__":
-    iterate_mailbox()
+    #iterate_mailbox()
+    threadify('example.mbox')
+
+
