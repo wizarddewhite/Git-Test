bff4c14a5e34a57c79ea0224a8f28d28f9e3a566
Wei Yang <richard.weiyang@gmail.com>
2019-03-24 19:59:08 +0800

    python: get_mail with message_id

--- userspace_app/python/mail/get_mail.py
+++ userspace_app/python/mail/get_mail.py
@@ -4,9 +4,9 @@ import mailbox
 if __name__ == "__main__":
     mbox = mailbox.mbox('example.mbox')
     mbox.lock()
-    for message in mbox:
+    for message_id, message in mbox.iteritems():
         if 'Re' in message['subject']:
             continue
-        print(message['subject'])
+        print("{:4}: {}".format(message_id, message['subject']))
     mbox.flush()
     mbox.close()
