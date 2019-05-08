b7f0b12ef082001887bc8abb1d3fdadd7f89dbc3
Wei Yang <richardw.yang@linux.intel.com>
2019-05-08 09:30:52 +0800

    qemu: migration with free page opt

--- userspace_app/qemu/migration.sh
+++ userspace_app/qemu/migration.sh
@@ -1,14 +1,14 @@
 # through tcp
 
-# source
+## source
 sudo ~/git/qemu/x86_64-softmmu/qemu-system-x86_64 -m 4G,slots=4,maxmem=128G -smp 4,maxcpus=16 --enable-kvm \
 	-drive file=/home/richard/guest/fedora29.img,format=raw -nographic \
 	-drive file=/home/richard/guest/project.img,format=raw \
 
-# type in monitor
+## type in monitor
 migrate -d tcp:0:4444
 
-# destination
+## destination
 sudo ~/git/qemu/x86_64-softmmu/qemu-system-x86_64 -m 4G,slots=4,maxmem=128G -smp 4,maxcpus=16 --enable-kvm \
 	-drive file=/home/richard/guest/fedora29.img,format=raw -nographic \
 	-drive file=/home/richard/guest/project.img,format=raw \
@@ -18,21 +18,25 @@ sudo ~/git/qemu/x86_64-softmmu/qemu-system-x86_64 -m 4G,slots=4,maxmem=128G -smp
 
 # through exec
 
-# source
+## source
 sudo ~/git/qemu/x86_64-softmmu/qemu-system-x86_64 -m 4G,slots=4,maxmem=128G -smp 4,maxcpus=16 --enable-kvm \
 	-drive file=/home/richard/guest/fedora29.img,format=raw -nographic \
 	-drive file=/home/richard/guest/project.img,format=raw \
 
-# type in monitor
+## type in monitor
 stop
 migrate "exec:cat > /your/back/file"
 quit
 
-# destination
+## destination
 sudo ~/git/qemu/x86_64-softmmu/qemu-system-x86_64 -m 4G,slots=4,maxmem=128G -smp 4,maxcpus=16 --enable-kvm \
 	-drive file=/home/richard/guest/fedora29.img,format=raw -nographic \
 	-drive file=/home/richard/guest/project.img,format=raw \
 	-incoming "exec:cat < /your/back/file"
 
-# type in monitor
+## type in monitor
 cont
+
+# free page opt
+
+-object iothread,id=iothread1 --device virtio-balloon,free-page-hint=true,iothread=iothread1
