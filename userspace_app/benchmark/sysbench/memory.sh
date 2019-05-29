0a3a91636e91b0c959a383539321f88ee9806364
Wei Yang <richardw.yang@linux.intel.com>
2019-05-29 09:57:21 +0800

    userspace/benchmard: sysbench set memory-block size

--- userspace_app/benchmark/sysbench/memory.sh
+++ userspace_app/benchmark/sysbench/memory.sh
@@ -1,3 +1,16 @@
-# memory
+# simple run
 
 sysbench memory run
+
+# get help
+
+sysbench memory help
+
+# use option
+
+## change block size
+
+this means the test will access 2M memory size instead of 1K by default
+
+sysbench --memory-block-size=2M memory run
+
