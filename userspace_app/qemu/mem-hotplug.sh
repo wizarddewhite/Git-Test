a5cfc7770c101cdf85ebd34f2599e63fc7198d2c
Wei Yang <richard.weiyang@gmail.com>
2019-06-16 09:04:47 +0800

    qemu/mem-hotplug: with node info

--- userspace_app/qemu/mem-hotplug.sh
+++ userspace_app/qemu/mem-hotplug.sh
@@ -7,4 +7,12 @@ sudo ~/git/qemu/x86_64-softmmu/qemu-system-x86_64 -m 4G,slots=4,maxmem=128G \
 object_add memory-backend-ram,id=ram0,size=1G
 device_add pc-dimm,id=dimm0,memdev=ram0
 
+# hotplug with node info
+sudo ~/git/qemu/x86_64-softmmu/qemu-system-x86_64 -m 4G,slots=4,maxmem=128G \
+	-smp 4,maxcpus=16 --enable-kvm \
+	-drive file=/home/richard/guest/fedora29.img,format=raw -nographic
+        -numa node,nodeid=0 -numa node,nodeid=1 -numa node,nodeid=2 -numa node,nodeid=3
 
+# execute in monitor
+object_add memory-backend-ram,id=ram0,size=1G
+device_add pc-dimm,id=dimm0,memdev=ram0,node=0
