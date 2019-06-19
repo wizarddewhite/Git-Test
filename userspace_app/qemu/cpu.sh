0d401397e1b46622163ca7ff1f08dfb6413c70c4
Wei Yang <richardw.yang@linux.intel.com>
2019-06-19 16:34:29 +0800

    qemu/cpu: how to specify cpuid

--- userspace_app/qemu/cpu.sh
+++ userspace_app/qemu/cpu.sh
@@ -1,3 +1,10 @@
+# show available cpu model and cpuid
+qemu-system-x86_64 -cpu help
+
 # specify cpu type
-# use host cpu type
+
+## use host cpu type
 -cpu host
+
+# specify cpuid with + or -
+-cpu host,+movdir64b,-movdiri
