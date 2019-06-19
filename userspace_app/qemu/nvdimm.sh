00b7e6238fc6206643ebb5565469da370171e4d6
Wei Yang <richardw.yang@linux.intel.com>
2019-06-19 08:52:40 +0800

    qemu/nvdimm: example on how to setup pmem

--- userspace_app/qemu/nvdimm.sh
+++ userspace_app/qemu/nvdimm.sh
@@ -7,3 +7,24 @@
 	-device nvdimm,id=nvdimm1,memdev=mem1,label-size=128k \
 	-object memory-backend-file,id=mem2,share=on,mem-path=/dev/dax0.1,size=2G,align=2M \
 	-device nvdimm,id=nvdimm2,memdev=mem2,label-size=128k
+
+In case backend is a pmem, we should configure like this:
+
+# lsblk
+NAME    MAJ:MIN RM   SIZE RO TYPE MOUNTPOINT
+pmem0   259:0    0  49.2G  0 disk
+
+# parted /dev/pmem0 mklabel gpt
+# parted -a opt /dev/pmem0 mkpart primary ext4 0% 100%
+# lsblk
+pmem0     259:0    0  49.2G  0 disk
+--pmem0p1 259:3    0  49.2G  0 part
+# mkfs.ext4 -L pmem1 /dev/pmem0p1
+# lsblk --fs
+pmem0
+--pmem0p1 ext4   pmem1 03d9f92d-0252-4163-bc5f-a9a5a78d62db
+
+# mount -o dax /dev/pmem0p1 /mnt
+# dd if=/dev/zero of=/mnt/backend1 bs=2G count=1
+
+-object memory-backend-file,id=mem1,share=on,mem-path=/mnt/backend1,size=2G,align=2M
