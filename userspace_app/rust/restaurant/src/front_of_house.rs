cfdf9ad15a9ce212f239238f7f7628a9dab94e27
Wei Yang <richardw.yang@linux.intel.com>
2019-06-30 11:05:15 +0800

    rust: define mod in separate file

---
+++ userspace_app/rust/restaurant/src/front_of_house.rs
@@ -0,0 +1,9 @@
+pub mod hosting {
+    pub fn add_to_waitlist() { }
+    fn seat_at_table() { }
+}
+mod serving {
+    fn take_over() {}
+    fn serve_order() {}
+    fn take_payment() {}
+}
--- userspace_app/rust/restaurant/src/lib.rs
+++ userspace_app/rust/restaurant/src/lib.rs
@@ -1,15 +1,4 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() { }
-        fn seat_at_table() { }
-    }
-    mod serving {
-        fn take_over() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
-}
-
+mod front_of_house;
 use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
