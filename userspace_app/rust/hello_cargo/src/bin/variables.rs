fa574e0d2e390b408b1d59a00785dad8f1cc23ce
Wei Yang <richardw.yang@linux.intel.com>
2019-07-01 05:53:07 +0800

    rust: explore variable

---
+++ userspace_app/rust/hello_cargo/src/bin/variables.rs
@@ -0,0 +1,26 @@
+#![allow(unused_variables)]
+const MAX_POINTS: u32 = 100_000;
+fn main() {
+    // immutable
+    let x = 5;
+    println!("The value of x is: {}", x);
+    // error: can't assign twice to immutable variables
+    //x = 6;
+    //println!("The value of x is: {}", x);
+
+    // mutable
+    let mut y = 5;
+    println!("The value of y is: {}", y);
+    y = 6;
+    println!("The value of y is: {}", y);
+
+    // shadow
+    let z = 5;
+    let z = z + 1;
+    let z = z * 2;
+    println!("The value of z is: {}", z);
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("The space length is: {}", spaces);
+}
