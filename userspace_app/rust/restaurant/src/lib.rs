c73d3ea1ad7d2d33999b16e1458e9e3490be7cd6
Wei Yang <richardw.yang@linux.intel.com>
2019-06-30 10:21:51 +0800

    rust: access module function

--- userspace_app/rust/restaurant/src/lib.rs
+++ userspace_app/rust/restaurant/src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() { }
+    pub mod hosting {
+        pub fn add_to_waitlist() { }
         fn seat_at_table() { }
     }
     mod serving {
@@ -9,4 +8,12 @@ mod front_of_house {
         fn serve_order() {}
         fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
