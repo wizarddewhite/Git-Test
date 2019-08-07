3712c7a5d96a21c0b8853f167788c2f5558368f6
Wei Yang <richardw.yang@linux.intel.com>
2019-08-07 15:32:32 +0800

    kernel_code_test/mm: copy find_vma_links

--- kernel_code_reading/code_test/mm/vma/vma.c
+++ kernel_code_reading/code_test/mm/vma/vma.c
@@ -145,3 +145,37 @@ void vma_link(struct mm_struct *mm, struct vm_area_struct *vma,
 	mm->map_count++;
 	validate_mm(mm);
 }
+
+int find_vma_links(struct mm_struct *mm, unsigned long addr,
+		unsigned long end, struct vm_area_struct **pprev,
+		struct rb_node ***rb_link, struct rb_node **rb_parent)
+{
+	struct rb_node **__rb_link, *__rb_parent, *rb_prev;
+
+	__rb_link = &mm->mm_rb.rb_node;
+	rb_prev = __rb_parent = NULL;
+
+	while (*__rb_link) {
+		struct vm_area_struct *vma_tmp;
+
+		__rb_parent = *__rb_link;
+		vma_tmp = rb_entry(__rb_parent, struct vm_area_struct, vm_rb);
+
+		if (vma_tmp->vm_end > addr) {
+			/* Fail if an existing vma overlaps the area */
+			if (vma_tmp->vm_start < end)
+				return -ENOMEM;
+			__rb_link = &__rb_parent->rb_left;
+		} else {
+			rb_prev = __rb_parent;
+			__rb_link = &__rb_parent->rb_right;
+		}
+	}
+
+	*pprev = NULL;
+	if (rb_prev)
+		*pprev = rb_entry(rb_prev, struct vm_area_struct, vm_rb);
+	*rb_link = __rb_link;
+	*rb_parent = __rb_parent;
+	return 0;
+}
