ca2ddf9c922054540a0d8efabf793ba323c8e72f
Wei Yang <richardw.yang@linux.intel.com>
2019-08-29 16:53:49 +0800

    python/mail: extract mail during a period to another mbox

---
+++ userspace_app/python/mail/extract_mail.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+from datetime import datetime, timedelta
+import mailbox
+from dateutil.parser import parse
+import re
+import argparse
+import calendar
+
+def move_mail(mbox_name, month):
+
+    _, ld = calendar.monthrange(month.year, month.month)
+    start = month.replace(day=1)
+    end =  month.replace(day=ld)
+    print "start: ", start
+    print "end  : ", end
+
+    mbox = mailbox.mbox(mbox_name)
+    mbox_new = mailbox.mbox(mbox_name + "_" + month.strftime('%Y%m'))
+    mbox.lock()
+
+    for idx, message in mbox.iteritems():
+        date = parse(re.sub('\(.*?\)', '', message['date'])).replace(tzinfo=None)
+        if date < start or date > end:
+            continue
+        #print message['subject']
+        mbox_new.add(message)
+        mbox.remove(idx)
+
+    mbox.flush()
+    mbox.close()
+    mbox_new.flush()
+    mbox_new.close()
+
+    return
+
+if __name__ == "__main__":
+    #iterate_mailbox()
+
+    parser = argparse.ArgumentParser(description='Processing mailbox')
+    parser.add_argument('--mailbox', metavar='path', required=True,
+                        help='the file of mailbox copy mail from')
+    parser.add_argument('--month', metavar='date', required=True,
+                        help='the month of mail to extract')
+    args = parser.parse_args()
+
+    if args.month:
+        month = parse(args.month)
+
+    move_mail(args.mailbox, month)
