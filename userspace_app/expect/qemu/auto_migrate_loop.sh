2070ec2381c86c42035582f037824aa3ab31831a
Wei Yang <richardw.yang@linux.intel.com>
2019-10-25 16:27:23 +0800

    expect/auto_migrate: print state message

--- userspace_app/expect/qemu/auto_migrate_loop.sh
+++ userspace_app/expect/qemu/auto_migrate_loop.sh
@@ -5,11 +5,15 @@ failures=0
 
 for i in $(eval echo "{1..$counts}")
 do
+	printf "[%2d]: " $i
 	expect -f ~/guest/auto_migrate ~/guest/qemu_command root 144025shyw &> .migrate_log
 
 	if [ $? -ne 0 ]
 	then
+		echo Failed
 		let failures++
+	else
+		echo Succeed
 	fi
 done
 
