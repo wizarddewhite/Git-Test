87c46c1866705d00454e1f7d100afb4ecf827b19
Wei Yang <richard.weiyang@gmail.com>
2019-12-08 20:42:43 +0800

    rust/struct: more description and cleanup

--- userspace_app/rust/hello_cargo/src/bin/struct.rs
+++ userspace_app/rust/hello_cargo/src/bin/struct.rs
@@ -1,4 +1,5 @@
 #[derive(Debug)]
+// define custom type with struct
 struct User {
     username: String,
     email: String,
@@ -7,46 +8,52 @@ struct User {
 }
 
 fn build_user(email: String, username: String) -> User {
+    // instance a variable with type struct User
     User {
         //email: email,
-        email,
+        email, // could be simplified since they have the same name
         username: username,
         active: true,
         sign_in_count: 1,
     }
 }
 
-// tuple struct
+// some example on struct type
+fn test_struct() {
+    // function with return value of type struct
+    let _user1 = build_user(String::from("someone@example.com"), 
+                           String::from("someone"));
+    let email = String::from("another@example.com");
+    let username = String::from("another");
+    let user2 = User {
+        email,
+        username,
+        // active: user1.active,
+        // sign_in_count: user1.sign_in_count,
+        .._user1  // up two lines be simplified for Struct Update syntax
+    };
+    println!("user2 is {:#?}", user2);
+}
+
+// tuple struct, a more verbose way to use tuple
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
 
-fn main() {
-    let _user1 = build_user(String::from("someone@emample.com"), 
-                           String::from("someone"));
-    let user2 = User {
-        email: String::from("another@example.com"),
-        username: String::from("another"),
-        .._user1
-    };
-    println!("user2 is {:#?}", user2);
-
-    let mut black = Color(0,0,0);
+fn test_tuple_struct() {
+    let black = Color(0, 0, 0);
     println!("Black.0 is {}", black.0);
-
-    let rect = Rectangle {width: 30, height: 50};
-    println!("Area of rectangle {}", rect.area());
-    let rect2 = Rectangle {width: 10, height: 40};
-    println!("Can rect hold rect2? {}", rect.can_hold(&rect2));
-    let sq = Rectangle::square(10);
-    println!("Area of square {}", sq.area());
+    let point = Point(1, 2, 3);
+    println!("Point.0 is {}", point.0);
 }
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 impl Rectangle {
+    // self must be the first parameter
     fn area(&self) -> u32 {
         self.width * self.height
     }
@@ -56,8 +63,29 @@ impl Rectangle {
     }
 }
 
+// define associated functions, no self is required
 impl Rectangle {
     fn square(size: u32) -> Rectangle {
         Rectangle {width: size, height: size}
     }
+}
+
+fn example_for_struct() {
+    let rect = Rectangle {width: 30, height: 50};
+    // print rect with Debug format
+    println!("rect is {:?}", rect);
+    // print rect with pretty Debug format
+    println!("rect is {:#?}", rect);
+    println!("Area of rectangle {}", rect.area());
+    let rect2 = Rectangle {width: 10, height: 40};
+    println!("Can rect hold rect2? {}", rect.can_hold(&rect2));
+    let sq = Rectangle::square(10);
+    println!("Area of square {}", sq.area());
+}
+
+fn main() {
+
+    test_struct();
+    test_tuple_struct();
+    example_for_struct();
 }
