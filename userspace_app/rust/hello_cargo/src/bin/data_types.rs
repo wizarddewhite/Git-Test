2a856a83463241b637510c91de9965f7459c49b9
Wei Yang <richard.weiyang@gmail.com>
2019-12-08 09:47:49 +0800

    rust/variable: add comment to explain type

--- userspace_app/rust/hello_cargo/src/bin/data_types.rs
+++ userspace_app/rust/hello_cargo/src/bin/data_types.rs
@@ -1,22 +1,45 @@
 fn main(){
+
+    // Scalar types: 
+    // * integers
+    // * floating-point numbers
+    // * Booleans
+    // * characters 
+
     // integer
     let _a: u32 = 10_000;
     let _b: u64 = 100_000;
+    println!("The value of a is {}", _a);
     // float
     let _c: f32 = 2.0;
     let _d: f64 = 3.0;
     // boolean
     let _e: bool = true;
-    // characer, 4 bytes
+    // character, 4 bytes
     let _f: char = 'z';
 
 
-    // tuple
+    // Compound types:
+    // * tuples 
+    // * arrays
+
+    // tuple, fixed length
     let tup: (i32, f64, u8) = (500, 6.4, 1);
+    // destruct tup into separate variable
     let (_, y, _) = tup;
     println!("The value of y is {}", y);
+    // access tup by using a period (.)
     println!("The value of y is {}", tup.1);
-    // array
+
+
+    // array, must has the same type and fixed length
+    // basic definition
+    let a = [1, 2, 3, 4, 5];
+    println!("The value of a[0] is {}", a[0]);
+    // specify type and length
+    let b: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("The value of b[0] is {}", b[0]);
+    // an array with 5 same value: "3"
     let arr: [i32; 5] = [3; 5];
     println!("The value of arr[0] is {}", arr[0]);
 }
