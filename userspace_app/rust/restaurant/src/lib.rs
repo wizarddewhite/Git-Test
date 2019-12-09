c6253556349ec688a261bd3744e3d8b14fca0c93
Wei Yang <richardw.yang@linux.intel.com>
2019-12-09 13:56:08 +0800

    rust/restaurant: access struct pub

--- userspace_app/rust/restaurant/src/lib.rs
+++ userspace_app/rust/restaurant/src/lib.rs
@@ -1,6 +1,32 @@
 mod front_of_house;
 use crate::front_of_house::hosting;
 
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
 }
