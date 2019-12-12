e664f6e85ef5bbdc6f52cd3c84030c486f1deac6
Wei Yang <richardw.yang@linux.intel.com>
2019-12-12 14:06:51 +0800

    rust/test_framework: use assert for test

--- userspace_app/rust/test_framework/src/lib.rs
+++ userspace_app/rust/test_framework/src/lib.rs
@@ -1,5 +1,20 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test] // indicate this is a test function
     fn it_works() {
         assert_eq!(2 + 2, 4);
@@ -8,5 +23,14 @@ mod tests {
     #[test]
     fn another() {
         panic!("Make this test fail");
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { width: 8, height: 7};
+        let smaller = Rectangle { width: 5, height: 1};
+
+        // use assert for check
+        assert!(larger.can_hold(&smaller));
     }
 }
