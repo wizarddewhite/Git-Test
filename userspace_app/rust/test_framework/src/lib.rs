9fca9a7f9cc9690fa3ff3812c0a5c2702b202e8e
Wei Yang <richardw.yang@linux.intel.com>
2019-12-12 14:25:41 +0800

    rust/test_framework: should_panic

--- userspace_app/rust/test_framework/src/lib.rs
+++ userspace_app/rust/test_framework/src/lib.rs
@@ -18,6 +18,20 @@ fn greeting(name: &str) -> String {
     format!("Hello !")
 }
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 ||  value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,5 +69,11 @@ mod tests {
             result.contains("Carol"),
             "Greeting did not contain name, value was {}", result
             );
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
