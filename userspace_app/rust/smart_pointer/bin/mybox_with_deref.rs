ca4732e313f341da28aa36395f4ac253133d080a
Wei Yang <richard.weiyang@gmail.com>
2019-12-15 07:06:59 +0800

    rust/smart_pointer: Deref trait for dereference

---
+++ userspace_app/rust/smart_pointer/bin/mybox_with_deref.rs
@@ -0,0 +1,28 @@
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+// implement Deref trait
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    // *y equals to *(y.deref())
+    assert_eq!(5, *y);
+    println!("{}", *y);
+}
