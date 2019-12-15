6608d638f303f97d1186ba11e5348c0ce1772411
Wei Yang <richard.weiyang@gmail.com>
2019-12-15 21:00:53 +0800

    rust/smart_pointer: RC and RefCell

--- userspace_app/rust/smart_pointer/bin/ref_count.rs
+++ userspace_app/rust/smart_pointer/bin/ref_count.rs
@@ -6,7 +6,7 @@ enum List {
 use crate::List::{Cons, Nil};
 use std::rc::Rc;
 
-fn main() {
+fn test_rc() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
     println!("count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
@@ -16,4 +16,39 @@ fn main() {
         println!("count after creating c = {}", Rc::strong_count(&a));
     }
     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+}
+
+#[derive(Debug)]
+enum List2 {
+    Cons2(Rc<RefCell<i32>>, Rc<List2>),
+    Nil2,
+}
+
+use crate::List2::{Cons2, Nil2};
+use std::cell::RefCell;
+
+fn test_rc_refcell() {
+    let value = Rc::new(RefCell::new(5));
+    println!("value.strong_count after creating = {}",
+             Rc::strong_count(&value));
+
+    let a = Rc::new(Cons2(Rc::clone(&value), Rc::new(Nil2)));
+    println!("value.strong_count after clone for a = {}",
+             Rc::strong_count(&value));
+
+    let b = Cons2(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons2(Rc::new(RefCell::new(10)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+}
+
+fn main() {
+    println!("### Test RC");
+    test_rc();
+    println!("### Test RC & RefCell");
+    test_rc_refcell();
 }
