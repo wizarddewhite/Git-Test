e39da53ef0c5b76d49848d896065b6d8f8a3e537
Wei Yang <richard.weiyang@gmail.com>
2019-12-16 21:53:36 +0800

    rust/concurrenty: channel

---
+++ userspace_app/rust/concurrency/src/bin/channel.rs
@@ -0,0 +1,84 @@
+use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
+
+fn single_value() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
+
+fn multiple_value() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            println!("{}", val);
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+fn multiple_producer() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("[1]hi"),
+            String::from("[1]from"),
+            String::from("[1]the"),
+            String::from("[1]thread"),
+        ];
+    
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("[2]more"),
+            String::from("[2]messages"),
+            String::from("[2]for"),
+            String::from("[2]you"),
+        ];
+    
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+fn main() {
+    println!("### single value");
+    single_value();
+    println!("### multiple value");
+    multiple_value();
+    println!("### multiple producer");
+    multiple_producer();
+}
