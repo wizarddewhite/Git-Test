ac0ed7ce16e9fb9bce6f98a1b102f09282698576
Wei Yang <richardw.yang@linux.intel.com>
2019-12-18 09:50:25 +0800

    rust/mgrep: split file read into run()

--- userspace_app/rust/mgrep/src/main.rs
+++ userspace_app/rust/mgrep/src/main.rs
@@ -14,9 +14,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
+    run(config);
 }
 
 struct Config {
@@ -38,4 +36,10 @@ impl Config {
             filename,
         })
     }
+}
+
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents);
 }
