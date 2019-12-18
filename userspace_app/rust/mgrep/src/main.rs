d5f09e4096d0c7219361decde4051ade1059c156
Wei Yang <richardw.yang@linux.intel.com>
2019-12-18 10:04:09 +0800

    rust/mgrep: split code into lib.rs

--- userspace_app/rust/mgrep/src/main.rs
+++ userspace_app/rust/mgrep/src/main.rs
@@ -1,13 +1,11 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let config = Config::new(&args).unwrap_or_else(|err|{
+    let config = mgrep::Config::new(&args).unwrap_or_else(|err|{
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
@@ -15,37 +13,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = mgrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {
-            query,
-            filename,
-        })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // return error value on error
-    let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
