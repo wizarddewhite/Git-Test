8be73cc8713772f405b4b4a2353b66c0c45fc24e
Wei Yang <richardw.yang@linux.intel.com>
2019-12-18 09:47:50 +0800

    rust/mgrep: improve argument parse error handling

--- userspace_app/rust/mgrep/src/main.rs
+++ userspace_app/rust/mgrep/src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -21,13 +25,17 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config {
+        Ok(Config {
             query,
             filename,
-        }
+        })
     }
 }
