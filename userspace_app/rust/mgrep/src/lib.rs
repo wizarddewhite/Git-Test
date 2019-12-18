e48ba88833603767a0eb5b7f06af9559fdcf8886
Wei Yang <richardw.yang@linux.intel.com>
2019-12-18 10:36:35 +0800

    rust/mgrep: add search and its test

--- userspace_app/rust/mgrep/src/lib.rs
+++ userspace_app/rust/mgrep/src/lib.rs
@@ -30,3 +30,33 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
