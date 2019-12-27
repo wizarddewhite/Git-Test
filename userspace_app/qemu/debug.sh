eba38487f248bc3543109e225e6399887c58b05a
Wei Yang <richard.weiyang@gmail.com>
2019-12-27 22:05:05 +0800

    qemu/debug: debug guest kernel with gdb

--- userspace_app/qemu/debug.sh
+++ userspace_app/qemu/debug.sh
@@ -16,9 +16,17 @@ source gdb-script
 b rom_add_blob
 r
 
-file vmlinux
+# guest kernel preparation
+# refer to
+# https://yulistic.gitlab.io/2018/12/debugging-linux-kernel-with-gdb-and-qemu/
+# https://unix.stackexchange.com/questions/396013/hardware-breakpoint-in-gdb-qemu-missing-start-kernel
+#
+# 1. Build kernel with CONFIG_DEBUG_INFO=y and CONFIG_RANDOMIZE_BASE=n
+# 2. Make symbol file: objcopy --only-keep-debug vmlinux kernel.sym, and copy to host
+# run gdb with following commands
+
+file kernel.sym
 target remote :1234
 set breakpoint pending on
-set auto-load safe-path /home/richard/git/linux
 hb start_kernel
 
