63c3039864b9a396f875d9d45f8505fe4845b1c0
Wei Yang <richard.weiyang@gmail.com>
2020-01-04 23:33:44 +0800

    user/adv: adjust some parameter
    wait longer for search menu and adv

--- userspace_app/bash/adv.sh
+++ userspace_app/bash/adv.sh
@@ -15,7 +15,7 @@
 locate_position() {
 # open menu
 adb shell input tap 1000 93
-sleep 0.5
+sleep 3
 # open search
 adb shell input tap 1000 1600
 sleep 0.5
@@ -31,7 +31,8 @@ locate_position
 
 # enter adv
 adb shell input tap 512 1400
-sleep 5
+echo enter adv
+sleep 20
 
 # back adv
 adb shell input keyevent 4
