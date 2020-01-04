084103e1ec52ed05b61c22306541c06662366d62
Wei Yang <richard.weiyang@gmail.com>
2020-01-05 00:00:20 +0800

    user/adv: better reading

--- userspace_app/bash/adv.sh
+++ userspace_app/bash/adv.sh
@@ -13,42 +13,42 @@
 #   adb shell ime set com.iflytek.inputmethod/.FlyIME
 
 locate_position() {
-# open menu
-adb shell input tap 1000 93
-sleep 3
-# open search
-adb shell input tap 1000 1600
-sleep 0.5
-adb shell am broadcast -a ADB_INPUT_TEXT --es msg '在看'
-sleep 2
+    # open menu
+    adb shell input tap 1000 93
+    sleep 3
+    # open search
+    adb shell input tap 1000 1600
+    sleep 0.5
+    adb shell am broadcast -a ADB_INPUT_TEXT --es msg '在看'
+    sleep 2
 }
 
 tap_one_artical() {
-#enter article
-adb shell input tap $1 $2
-
-locate_position
-
-# enter adv
-echo enter adv
-adb shell input tap 512 1300
-sleep 20
-
-# back adv
-adb shell input keyevent 4
-# back article
-adb shell input tap 95 93
-sleep 5
+    #enter article
+    adb shell input tap $1 $2
+    
+    locate_position
+    
+    # enter adv
+    echo enter adv
+    adb shell input tap 512 1300
+    sleep 20
+    
+    # back adv
+    adb shell input keyevent 4
+    # back article
+    adb shell input tap 95 93
+    sleep 5
 }
 
 for (( ; ; ))
 do
 
-echo Tap one article
-tap_one_artical 512 1520
-
-# move up one
-adb shell input swipe 500 860 500 300
-sleep 2
+    echo Tap one article
+    tap_one_artical 512 1520
+    
+    # move up one
+    adb shell input swipe 500 860 500 300
+    sleep 2
 
 done
