9e67504dc10405ae483ffa2cf28ee701312cfe2d
Wei Yang <richard.weiyang@gmail.com>
2020-01-04 22:10:41 +0800

    user/adv: first working version

---
+++ userspace_app/bash/adv.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# adb start-server
+
+# https://blog.bihe0832.com/adb-shell-input.html
+
+for (( ; ; ))
+do
+#enter article
+adb shell input tap 512 1520
+
+# scroll to top
+adb shell input swipe 500 300 500 1800
+adb shell input swipe 500 300 500 1800
+
+# scroll down to point
+adb shell input swipe 500 1600 500 200
+
+sleep 2
+
+# enter adv
+adb shell input tap 512 1520
+sleep 5
+
+# back adv
+adb shell input keyevent 4
+# back article
+adb shell input keyevent 4
+sleep 10
+done
