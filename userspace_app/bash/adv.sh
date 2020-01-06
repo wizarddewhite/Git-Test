e10b4b4b61e9d50f68bfbcc204e40835b0fd3560
Wei Yang <richard.weiyang@gmail.com>
2020-01-06 22:06:58 +0800

    user/adv: randomize up action

--- userspace_app/bash/adv.sh
+++ userspace_app/bash/adv.sh
@@ -68,9 +68,13 @@ to_subscribe_list() {
 }
 
 next_article() {
+    times=$((1 + RANDOM % 3))
+    for i in $(eval echo "{1..$times}")
+    do
         # move up one
         adb shell input swipe 500 860 500 302
-        sleep 1
+    done
+    sleep 1
 }
 
 for (( ; ; ))
