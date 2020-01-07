f99ba8795e02ce46085c53bf25948be35536008f
Wei Yang <richardw.yang@linux.intel.com>
2020-01-07 10:49:40 +0800

    user/adv: move icon to left top

--- userspace_app/bash/adv.sh
+++ userspace_app/bash/adv.sh
@@ -85,7 +85,7 @@ restart_app() {
     adb shell am force-stop com.tencent.mm
     sleep 0.5
     # need to put icon to this point
-    adb shell input tap 200 1300
+    adb shell input tap 200 300
     sleep 10 # wait for launch
 }
 
