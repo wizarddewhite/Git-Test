5b2ed12025e4e77cd2b8e409e8891cdf15ae65af
Wei Yang <richardw.yang@linux.intel.com>
2020-01-09 10:30:49 +0800

    user/adv: tap input box before input

--- userspace_app/bash/adv/login.sh
+++ userspace_app/bash/adv/login.sh
@@ -14,10 +14,13 @@ log_in() {
     sleep 2
 
     # log in
-    adb shell am broadcast -a ADB_INPUT_TEXT --es msg ${3}
+    input=`echo "($height*0.364)/1" | bc`
     y=`echo "($height*0.572)/1" | bc`
+    adb shell input tap $x $input
+    adb shell am broadcast -a ADB_INPUT_TEXT --es msg ${3}
     adb shell input tap $x $y
     sleep 8
+    adb shell input tap $x $input
     adb shell am broadcast -a ADB_INPUT_TEXT --es msg '144025shyw'
     adb shell input tap $x $y
     sleep 8
