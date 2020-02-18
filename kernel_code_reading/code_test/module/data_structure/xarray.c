d794731d19b2fcca97a3bf5d72ad77a704614e15
Wei Yang <richardw.yang@linux.intel.com>
2020-02-18 10:46:22 +0800

    kernel/code_test: xarray xas example

--- kernel_code_reading/code_test/module/data_structure/xarray.c
+++ kernel_code_reading/code_test/module/data_structure/xarray.c
@@ -15,7 +15,7 @@ void test_store_load(void)
 		printk(KERN_ERR "index 1 has value %lu\n", xa_to_value(entry));
 	else
 		printk(KERN_ERR "index 1 has no value\n");
-	printk("xa is %sempty\n", xa_empty(&array)?"":"not ")
+	printk("xa is %sempty\n", xa_empty(&array)?"":"not ");
 
 	xa_destroy(&array);
 	printk("destroy xarray\n");
@@ -24,11 +24,35 @@ void test_store_load(void)
 		printk(KERN_ERR "index 1 has value %lu\n", xa_to_value(entry));
 	else
 		printk(KERN_ERR "index 1 has no value\n");
+	printk("xa is %sempty\n", xa_empty(&array)?"":"not ");
+}
+
+void test_xas(void)
+{
+	void *entry;
+	XA_STATE(xas, &array, 0);
+
+	xa_store(&array, 1, xa_mk_value(1), GFP_KERNEL);
+	xa_store(&array, 2, xa_mk_value(2), GFP_KERNEL);
+ 	/* xas_find() - Find the next present entry in the XArray. */
+	entry = xas_find(&xas, ULONG_MAX);
+	printk(KERN_ERR "the first entry has value %lu\n", xa_to_value(entry));
+	entry = xas_next_entry(&xas, ULONG_MAX);
+	printk(KERN_ERR "the second entry has value %lu\n", xa_to_value(entry));
+
+	/* replace index 2 with value 5 */
+	xas_store(&xas, xa_mk_value(5));
+
+	xas_set(&xas, 0);
+	xas_for_each(&xas, entry, ULONG_MAX)
+		printk(KERN_ERR "entry %lu\n", xa_to_value(entry));
+
 }
 
 static int xarry_test_init(void)
 {
 	test_store_load();
+	test_xas();
 	return 0;
 }
 static void xarry_test_exit(void)
