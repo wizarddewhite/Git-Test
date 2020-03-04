cb05cb515bf51f3a626d7b823477f77d12dfe9ee
Wei Yang <richard.weiyang@gmail.com>
2020-03-04 09:41:32 +0000

    userspace/go: start from a fork

---
+++ userspace_app/go/one_simple_container/my_container.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("what?")
+	}
+}
+
+func run() {
+	fmt.Println("runnig ", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
+}
+
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
