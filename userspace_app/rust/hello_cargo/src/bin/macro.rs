8e212ea9c78ec984bf9cd1b28078b7f41451514a
Wei Yang <richard.weiyang@linux.alibaba.com>
2020-05-25 14:56:49 +0800

    userspace/rust: cargo expand for better understanding

--- userspace_app/rust/hello_cargo/src/bin/macro.rs
+++ userspace_app/rust/hello_cargo/src/bin/macro.rs
@@ -11,8 +11,22 @@ macro_rules! my_vec {
     };
 }
 
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $( map.insert($key, $val); )*
+         map
+    }}
+}
+
+// cargo expand --bin macro
+// would expand the definition, which is helpful for understanding the code
+
 fn main() {
     let v: Vec<u32> = my_vec![1, 2, 3];
     println!("Test on Macro");
     println!("My vec: {:?}", v);
+
+    let counts = hashmap!['A' => 0, 'C' => 0, 'G' => 0, 'T' => 0];
+    println!("{:?}", counts);
 }
