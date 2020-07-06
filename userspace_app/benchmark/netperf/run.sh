13cf24ac941d1d4b500a2cba1bf40936a52625a3
Wei Yang <richard.weiyang@linux.alibaba.com>
2020-07-06 09:59:32 +0800

    userspace/benchmark: some examples for netperf and fio

--- userspace_app/benchmark/netperf/run.sh
+++ userspace_app/benchmark/netperf/run.sh
@@ -2,4 +2,16 @@
 netserver &
 
 # client
+
+# RR for latency, STREAM for qps
 netperf -H server_ip -t tcp_rr -l 60
+
+netperf -H $SERVER_IP -t TCP_RR -l 120 -- -l 1,1
+netperf -H $SERVER_IP -t TCP_RR -l 120 -- -l 32,1024
+netperf -H $SERVER_IP -t TCP_STREAM -l 120 -- -l 1,1
+netperf -H $SERVER_IP -t TCP_STREAM -l 120 -- -l 32,1024
+
+netperf -H $SERVER_IP -t UDP_RR -l 120 -- -l 1,1
+netperf -H $SERVER_IP -t UDP_RR -l 120 -- -l 32,1024
+netperf -H $SERVER_IP -t UDP_STREAM -l 120 -- -l 1,1
+netperf -H $SERVER_IP -t UDP_STREAM -l 120 -- -l 32,1024
