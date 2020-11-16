eb5b871700c752c6d5e7079bab551e1a2b6cda07
Wei Yang <richard.weiyang@gmail.com>
2020-11-16 22:07:15 +0800

    userspace/go: parse json from file

---
+++ userspace_app/go/json/parse_file.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	// "io/ioutil"
+	"os"
+)
+
+type Setting struct {
+	ID string `json:"id"`
+}
+
+type Config struct {
+	Port     uint16  `json:"port"`
+	Settings Setting `json:"settings"`
+}
+
+func simple_test() {
+	file, _ := os.Open("json.conf")
+	config := &Config{}
+	decoder := json.NewDecoder(file)
+	decoder.Decode(config)
+	fmt.Println(config.Port)
+	fmt.Println(config.Settings.ID)
+}
+
+func main() {
+	simple_test()
+}
