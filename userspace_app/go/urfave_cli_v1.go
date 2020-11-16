f717fd107dbd435f98683d11381835612b5d2000
Wei Yang <richard.weiyang@gmail.com>
2020-11-16 14:31:43 +0800

    userspace/go: add a sub-command

--- userspace_app/go/urfave_cli_v1.go
+++ userspace_app/go/urfave_cli_v1.go
@@ -14,32 +14,36 @@ var usage = fmt.Sprintf(`%s runtime
 %s is a command line program for running applications packaged
 according to the Open Container Initiative (OCI).`, "cli", "cli")
 
+var cliCommands = []cli.Command{
+	{
+		Name:  "hello",
+		Usage: "Say Hello",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "lang",
+				Value: "english",
+				Usage: "language for greeting",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			name := "Nefertiti"
+			if c.String("lang") == "spanish" {
+				fmt.Println("Hola", name)
+			} else {
+				fmt.Println("Hello", name)
+			}
+
+			return nil
+		},
+	},
+}
+
 func main() {
 	app := cli.NewApp()
 
 	app.Name = "cli test"
 	app.Usage = usage
-
-	app.Flags = []cli.Flag{
-		&cli.StringFlag{
-			Name:  "lang",
-			Value: "english",
-			Usage: "language for the greeting",
-		},
-	}
-
-	app.Action = func(c *cli.Context) error {
-		name := "Nefertiti"
-		if c.NArg() > 0 {
-			name = c.Args().Get(0)
-		}
-		if c.String("lang") == "spanish" {
-			fmt.Println("Hola", name)
-		} else {
-			fmt.Println("Hello", name)
-		}
-		return nil
-	}
+	app.Commands = cliCommands
 
 	err := app.Run(os.Args)
 	if err != nil {
