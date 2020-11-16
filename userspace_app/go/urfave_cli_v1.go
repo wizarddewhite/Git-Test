88db06952f178cf897df774af4ec3e621b7399a2
Wei Yang <richard.weiyang@gmail.com>
2020-11-16 11:36:42 +0800

    userspace/go: urfave/cli: add a string flag

---
+++ userspace_app/go/urfave_cli_v1.go
@@ -0,0 +1,40 @@
+// https://github.com/urfave/cli/blob/master/docs/v1/manual.md
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:  "lang",
+			Value: "english",
+			Usage: "language for the greeting",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		name := "Nefertiti"
+		if c.NArg() > 0 {
+			name = c.Args().Get(0)
+		}
+		if c.String("lang") == "spanish" {
+			fmt.Println("Hola", name)
+		} else {
+			fmt.Println("Hello", name)
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
