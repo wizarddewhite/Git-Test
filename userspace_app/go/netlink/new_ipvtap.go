02e565943c3352562f05fbb4491d2d5be0e3d310
Wei Yang <richard.weiyang@gmail.com>
2020-12-01 17:26:57 +0800

    userspace/go: netlink add an interface into NS

--- userspace_app/go/netlink/new_ipvtap.go
+++ userspace_app/go/netlink/new_ipvtap.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/vishvananda/netlink"
+	"os"
 )
 
 func main() {
+	fd, err := os.Open("/var/run/netns/cni-9853c418-abae-27d7-66b3-c584af24302b")
+	if err != nil {
+		fmt.Println("Failed to open netns fd ", err)
+		return
+	}
+
 	mv := &netlink.IPVtap{netlink.IPVlan{
 		LinkAttrs: netlink.LinkAttrs{
 			MTU:         1500,
@@ -14,8 +21,11 @@ func main() {
 		},
 		Mode: netlink.IPVLAN_MODE_L2,
 	}}
-	err := netlink.LinkAdd(mv)
+
+	err = netlink.LinkAdd(mv)
 	if err != nil {
 		fmt.Printf("could not add %s: %v\n", "foo", err)
 	}
+
+	netlink.LinkSetNsFd(mv, int(fd.Fd()))
 }
