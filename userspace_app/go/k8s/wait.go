8dc75392c40cb9b2f3c70a782f87a718d86334a4
Wei Yang <richard.weiyang@gmail.com>
2020-12-24 16:19:24 +0800

    userspace/k8s: wait Until
    Execute function periodically.

---
+++ userspace_app/go/k8s/wait.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/klog"
+	"k8s.io/sample-controller/pkg/signals"
+)
+
+func process() {
+	fmt.Println("Processing...")
+}
+
+func main() {
+	stopCh := signals.SetupSignalHandler()
+
+	// run process() every 4 seconds
+	klog.Info("Starting process")
+	wait.Until(process, 2*time.Second, stopCh)
+}
