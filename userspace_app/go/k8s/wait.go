9a3a6b1df4e8ca372aca8bc0816a3676c67e0b87
Wei Yang <richard.weiyang@gmail.com>
2020-12-24 17:10:39 +0800

    userspace/k8s: adjust for futher examples

--- userspace_app/go/k8s/wait.go
+++ userspace_app/go/k8s/wait.go
@@ -9,17 +9,27 @@ import (
 	"k8s.io/sample-controller/pkg/signals"
 )
 
+var (
+	stopCh <-chan struct{}
+)
+
 func process() {
 	fmt.Println("Processing...")
 }
 
-func main() {
-	stopCh := signals.SetupSignalHandler()
-
+func until() {
 	// run process() every 4 seconds
 	klog.Info("Starting process")
 	go wait.Until(process, 2*time.Second, stopCh)
 
 	<-stopCh
 	klog.Info("Receive signal and quit")
+}
+
+func main() {
+	until()
+}
+
+func init() {
+	stopCh = signals.SetupSignalHandler()
 }
