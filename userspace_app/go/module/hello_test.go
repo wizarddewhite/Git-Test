a936c38f07c9eb787f444a7d0f5dac354a490d3c
Wei Yang <richard.weiyang@gmail.com>
2021-01-25 16:59:50 +0800

    userspace/go: go module
    The simple test without module.
    
    $ go test
    PASS
    ok  	_/home/richard/git/Git-Test/userspace_app/go/module	0.001s

---
+++ userspace_app/go/module/hello_test.go
@@ -0,0 +1,11 @@
+// https://blog.golang.org/using-go-modules
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	want := "Hello, world."
+	if got := Hello(); got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+}
