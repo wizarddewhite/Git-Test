d677016f0abb33292c37cde2220bd7a28442095d
Wei Yang <richard.weiyang@gmail.com>
2021-02-09 15:31:59 +0800

    userspace/go: adjust two dimention array

--- userspace_app/go/tour-go/array.go
+++ userspace_app/go/tour-go/array.go
@@ -38,6 +38,53 @@ func test_on_array() {
 	methodOnArray()
 }
 
+func dump_two_dim_array(array [][]int) {
+	row := len(array)
+	column := len(array[0])
+
+	fmt.Printf("array: %p, row: %d, column: %d\n", &array, row, column)
+	for i := 0; i < row; i++ {
+		fmt.Print("\t")
+		for j := 0; j < column; j++ {
+			fmt.Print(array[i][j])
+		}
+		fmt.Println()
+	}
+}
+
+func adjust_two_dim_array(array [][]int, row, column int) [][]int {
+	curr_row := len(array)
+	curr_column := len(array[0])
+	fmt.Printf("Adjust array from %dx%d to %dx%d\n",
+		curr_row, curr_column, row, column)
+
+	for i := 0; i < curr_row; i++ {
+		// adjust column
+		if column > curr_column {
+			// grow column
+			array[i] = append(array[i], make([]int, column-curr_column)...)
+		} else if column < curr_column {
+			// shrink column
+			array[i] = array[i][:column]
+		}
+	}
+
+	if row > curr_row {
+		// grow row
+		array = append(array, make([][]int, row-curr_row)...)
+		// allocate column
+		for i := curr_row; i < row; i++ {
+			array[i] = make([]int, column)
+		}
+	} else if row < curr_row {
+		// shrink row
+		array = array[:row]
+	}
+
+	dump_two_dim_array(array)
+	return array
+}
+
 func test_two_dim_array() {
 	n := 2
 	m := 3
@@ -54,12 +101,14 @@ func test_two_dim_array() {
 		012
 		345
 	*/
-	for i := 0; i < n; i++ {
-		for j := 0; j < m; j++ {
-			fmt.Print(grid[i][j])
-		}
-		fmt.Println()
-	}
+	dump_two_dim_array(grid)
+
+	grid = adjust_two_dim_array(grid, 2, 4)
+	grid = adjust_two_dim_array(grid, 2, 2)
+	grid = adjust_two_dim_array(grid, 1, 2)
+
+	// dump_two_dim_array(grid)
+	adjust_two_dim_array(grid, 3, 2)
 }
 
 func main() {
