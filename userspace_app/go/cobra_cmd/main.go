39ca10c3ba9c2bbae22ae006a76728c0dba0903b
Wei Yang <richard.weiyang@gmail.com>
2021-02-10 15:12:04 +0800

    userspace/go: add two command with arg validation

--- userspace_app/go/cobra_cmd/main.go
+++ userspace_app/go/cobra_cmd/main.go
@@ -4,12 +4,14 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
 var (
 	nType   string
+	Region  string
 	dynamic bool
 )
 
@@ -21,6 +23,43 @@ var versionCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
 	},
+}
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Print the all elements",
+	Long:  `All software has versions. This is Hugo's`,
+	Args:  cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("list all elements on ", Region)
+	},
+}
+
+var restartCmd = &cobra.Command{
+	Use:   "restart",
+	Short: "Restart some element",
+	Long:  `Restart some element`,
+	Args: func(c *cobra.Command, args []string) error {
+		if len(args) != 3 {
+			return errors.New("restart require vm_name row column")
+		}
+
+		if _, err := strconv.Atoi(args[1]); err != nil {
+			return fmt.Errorf("%v should be a number", args[1])
+		}
+
+		if _, err := strconv.Atoi(args[2]); err != nil {
+			return fmt.Errorf("%v should be a number", args[2])
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("restart elements on ", Region)
+	},
+}
+
+func init() {
+	listCmd.Flags().StringVarP(&Region, "region", "r", "", "AWS region (required)")
 }
 
 func newCommand() *cobra.Command {
@@ -46,6 +85,8 @@ func newCommand() *cobra.Command {
 	flags.BoolVar(&dynamic, "dynamic", false, "Dynamic config")
 
 	cmd.AddCommand(versionCmd)
+	cmd.AddCommand(listCmd)
+	cmd.AddCommand(restartCmd)
 
 	return cmd
 }
