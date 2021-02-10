a18457884ba7144f8a0fb3eb4e1e35705cb13c9f
Wei Yang <richard.weiyang@gmail.com>
2021-02-10 14:31:15 +0800

    userspace/go: limit args for command version

--- userspace_app/go/cobra_cmd/main.go
+++ userspace_app/go/cobra_cmd/main.go
@@ -16,6 +16,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of Hugo",
 	Long:  `All software has versions. This is Hugo's`,
+	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
 	},
