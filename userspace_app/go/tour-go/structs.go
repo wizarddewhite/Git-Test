01afac72dc57246460f152b19a65882f6f2a5c66
Wei Yang <richard.weiyang@gmail.com>
2021-02-23 21:30:33 +0800

    userspace/go: struct embed field

--- userspace_app/go/tour-go/structs.go
+++ userspace_app/go/tour-go/structs.go
@@ -7,7 +7,37 @@ type Vertex struct {
 	Y int
 }
 
-func main() {
+func print_struct() {
 	fmt.Println(Vertex{1, 2})
 	fmt.Println(Vertex{X: 2, Y: 1})
+}
+
+type Meta struct {
+	Name string
+}
+
+type Type struct {
+	// Name string
+	Kind string
+}
+
+type EmbedField struct {
+	Type
+	Meta
+}
+
+func embeded_field() {
+	var ef EmbedField
+
+	ef.Name = "embed"
+	ef.Meta.Name = "embed"
+
+	ef.Kind = "field"
+
+	fmt.Println(ef)
+}
+
+func main() {
+	// print_struct()
+	embeded_field()
 }
