bf162ac36985ef16350c71cbeab5bbfd02982425
Wei Yang <richard.weiyang@gmail.com>
2021-03-15 11:51:32 +0800

    userspace/go: unix grpc example

--- userspace_app/go/grpc/client.go
+++ userspace_app/go/grpc/client.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"net"
+	"time"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -9,9 +11,15 @@ import (
 	"github.com/tutorialedge/go-grpc-tutorial/chat"
 )
 
+func UnixConnect(addr string, t time.Duration) (net.Conn, error) {
+	unix_addr, err := net.ResolveUnixAddr("unix", "grpc.sock")
+	conn, err := net.DialUnix("unix", nil, unix_addr)
+	return conn, err
+}
+
 func main() {
 	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	conn, err := grpc.Dial("grpc.sock", grpc.WithInsecure(), grpc.WithDialer(UnixConnect))
 	if err != nil {
 		log.Fatalf("could not connect: %s", err)
 	}
--- userspace_app/go/grpc/server.go
+++ userspace_app/go/grpc/server.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":9000")
+	lis, err := net.Listen("unix", "grpc.sock")
 	if err != nil {
 		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
