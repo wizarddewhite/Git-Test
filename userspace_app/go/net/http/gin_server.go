4a1355fcb38bb066b00129eb747a01cbdcf2a1ef
Wei Yang <richard.weiyang@gmail.com>
2021-04-06 11:54:53 +0800

    userspace/go: use gin package for http server

---
+++ userspace_app/go/net/http/gin_server.go
@@ -0,0 +1,19 @@
+// https://github.com/gin-gonic/gin
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": c.FullPath(),
+		})
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
