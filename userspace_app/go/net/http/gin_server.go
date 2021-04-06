4e94a281b12ee9a050b4bc237308004d7ef2da2c
Wei Yang <richard.weiyang@gmail.com>
2021-04-06 14:36:25 +0800

    userspace/go: several ways to use gin

--- userspace_app/go/net/http/gin_server.go
+++ userspace_app/go/net/http/gin_server.go
@@ -1,9 +1,14 @@
 // https://github.com/gin-gonic/gin
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net"
+	"net/http"
+	"time"
+)
 
-func main() {
+func simple_one() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -16,4 +21,84 @@ func main() {
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func config_http() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": c.FullPath(),
+		})
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	// r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
+
+func config_http2() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": c.FullPath(),
+		})
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	l, _ := net.Listen("tcp", ":8080")
+	// r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.Serve(l)
+}
+
+func raw_gin() {
+	r := gin.New()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": c.FullPath(),
+		})
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	l, _ := net.Listen("tcp", ":8080")
+	// r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.Serve(l)
+}
+
+func main() {
+	// simple_one()
+	// config_http()
+	// config_http2()
+	raw_gin()
 }
