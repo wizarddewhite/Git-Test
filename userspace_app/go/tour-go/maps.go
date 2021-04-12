63c307e438bc0ecf3c3338a21e24c3cd8afd2000
Wei Yang <richard.weiyang@gmail.com>
2021-04-12 11:16:28 +0800

    userspace/go: nil map can't assign

--- userspace_app/go/tour-go/maps.go
+++ userspace_app/go/tour-go/maps.go
@@ -10,6 +10,14 @@ type Vertex struct {
 
 func main() {
 	var m map[string]Vertex
+	if m == nil {
+		fmt.Println("after definition map is nil, and following would crash")
+		/*
+			m["Bell Labs"] = Vertex{
+				40.68433, -74.39967,
+			}
+		*/
+	}
 	m = make(map[string]Vertex)
 	m["Bell Labs"] = Vertex{
 		40.68433, -74.39967,
