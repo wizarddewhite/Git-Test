a8e8681668e6985ce331f499042887c3b5901cbb
Wei Yang <richard.weiyang@gmail.com>
2021-05-18 06:48:59 +0800

    v2ray: fix tls renew script

--- userspace_app/v2ray/acm-renew.sh
+++ userspace_app/v2ray/acm-renew.sh
@@ -1,4 +1,4 @@
-~/.acme.sh/acme.sh --issue -d freedom.tk --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d freedom.tk --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 stop bee
+~/.acme.sh/acme.sh --issue -d freedomland.tk --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d freedomland.tk --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 service nginx restart
