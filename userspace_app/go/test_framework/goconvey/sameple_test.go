215b8de1b15469f0192fba970a400c1b82c42524
Wei Yang <richard.weiyang@gmail.com>
2021-05-19 15:42:56 +0800

    userspace/go: category test cases by goconvey

--- userspace_app/go/test_framework/goconvey/sameple_test.go
+++ userspace_app/go/test_framework/goconvey/sameple_test.go
@@ -6,8 +6,23 @@ import (
 )
 
 func TestCheckUrl(t *testing.T) {
-	convey.Convey("TestCheckTeachUrl", t, func() {
+	convey.Convey("TestCheckUrl", t, func() {
 		ok := CheckUrl("learnku.com")
 		convey.So(ok, convey.ShouldBeTrue)
+	})
+
+	convey.Convey("TestCheckTeachUrl", t, func() {
+		convey.Convey("TestCheckTeachUrl true", func() {
+			ok := CheckUrl("learnku.com")
+			convey.So(ok, convey.ShouldBeTrue)
+		})
+		convey.Convey("TestCheckTeachUrl false", func() {
+			ok := CheckUrl("xxxxxx.com")
+			convey.So(ok, convey.ShouldBeFalse)
+		})
+		convey.Convey("TestCheckTeachUrl null", func() {
+			ok := CheckUrl("")
+			convey.So(ok, convey.ShouldBeFalse)
+		})
 	})
 }
