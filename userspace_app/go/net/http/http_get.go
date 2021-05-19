5fe8918ce05efe2445f7134b2f6194717646fb44
Wei Yang <richard.weiyang@gmail.com>
2021-05-19 20:51:37 +0800

    userspace/go: merge into http_get

--- userspace_app/go/net/http/http_get.go
+++ userspace_app/go/net/http/http_get.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
 	"os"
 	"strings"
 )
@@ -51,11 +52,78 @@ func raw_get() {
 	os.Exit(0)
 }
 
+func getCharset(response *http.Response) string {
+	contentType := response.Header.Get("Content-Type")
+	if contentType == "" {
+		// guess
+		return "UTF-8"
+	}
+	idx := strings.Index(contentType, "charset")
+	if idx == -1 {
+		// guess
+		return "UTF-8"
+	}
+	strs := strings.Split(contentType[idx:], "=")
+	if len(strs) == 2 {
+		return strings.ToUpper(strs[1])
+	}
+
+	return "UTF-8"
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
+
+func http_client() {
+	url, err := url.Parse(os.Args[1])
+	checkError(err)
+
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", url.String(), nil)
+	checkError(err)
+	request.Header.Add("Accept-Charset", "UTF-8;q=1, ISO-8859-1;q=0")
+
+	// send http request and get response
+	response, err := client.Do(request)
+	if response.Status != "200 OK" {
+		fmt.Println(response.Status)
+		os.Exit(2)
+	}
+
+	fmt.Println("====\nResponse Head:\n====")
+	b, _ := httputil.DumpResponse(response, true)
+	fmt.Print(string(b))
+	chSet := getCharset(response)
+	fmt.Printf("got charset: %s\n", chSet)
+	if chSet != "UTF-8" {
+		fmt.Println("Cannot handle", chSet)
+		os.Exit(4)
+	}
+
+	var buf [512]byte
+	reader := response.Body
+	fmt.Println("====\nResponse Body:\n====")
+	for {
+		n, err := reader.Read(buf[0:])
+		if err != nil {
+			os.Exit(0)
+		}
+		fmt.Print(string(buf[0:n]))
+	}
+
+	os.Exit(0)
+}
+
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Usage: ", os.Args[0], "http://host:port")
+		fmt.Println("Usage: ", os.Args[0], "http://host:port[/page]")
 		os.Exit(1)
 	}
 
-	raw_get()
+	// raw_get()
+	http_client()
 }
