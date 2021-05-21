c5b13ea214bd7b12b68f18ef122f08d9b36cff2f
Wei Yang <richard.weiyang@gmail.com>
2021-05-21 11:10:03 +0800

    userspace/go: use ValueOf to retrieve value

--- userspace_app/go/tour-go/reflect.go
+++ userspace_app/go/tour-go/reflect.go
@@ -5,6 +5,22 @@ import (
 	"reflect"
 )
 
+func getField() {
+	s := struct {
+		A int
+		B int
+	}{1, 2}
+	// print the value 1
+	field := reflect.ValueOf(s).Field(0)
+	fmt.Println(field.Interface())
+	// print the value 2
+	field = reflect.ValueOf(s).Field(1)
+	fmt.Println(field.Interface())
+
+	// print the whole struct value
+	fmt.Println(reflect.ValueOf(s))
+}
+
 func main() {
 
 	var a int
@@ -14,4 +30,6 @@ func main() {
 	// new a value from Type
 	val := reflect.Zero(intType)
 	fmt.Println(val)
+
+	getField()
 }
