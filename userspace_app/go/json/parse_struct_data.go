2a1ebe53ea29b28f2808d15f71d20c5ab8df9030
Wei Yang <richard.weiyang@gmail.com>
2021-06-21 16:46:54 +0800

    userspace/go: merge json parse into one file

--- userspace_app/go/json/parse_struct_data.go
+++ userspace_app/go/json/parse_struct_data.go
@@ -5,6 +5,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"time"
 )
 
 type Bird struct {
@@ -59,9 +61,54 @@ func custom_attr_name() {
 	fmt.Println(bird)
 }
 
+type Setting struct {
+	ID string `json:"id"`
+}
+type Process struct {
+	User string `json:"user"`
+}
+
+type Config struct {
+	Setting
+	Port     uint16   `json:"port"`
+	Settings Setting  `json:"settings"`
+	Process  *Process `json:"process,omitempty"`
+}
+
+func simple_test() {
+	file, _ := os.Open("json.conf")
+	config := &Config{}
+	decoder := json.NewDecoder(file)
+	decoder.Decode(config)
+	fmt.Println("ID:", config.Setting.ID)
+	fmt.Println("Port:", config.Port)
+	fmt.Println("Setting:", config.Settings.ID)
+	fmt.Println("Process:", config.Process.User)
+}
+
+type ObjStatus struct {
+	Status string `json:"status,omitempty"`
+	Reason string `json:"reason,omitempty"`
+
+	Source string    `json:"source,omitempty"`
+	Ts     time.Time `json:"timestamp,omitempty"`
+}
+
+func timestamp_parse() {
+	file, _ := os.Open("json_time.status")
+	status := &ObjStatus{}
+	decoder := json.NewDecoder(file)
+	decoder.Decode(status)
+	fmt.Println(status.Status)
+	fmt.Println(status.Ts.Format(time.RFC1123))
+	fmt.Println(status.Ts.Format("2006-01-02 15:04:05"))
+}
+
 func main() {
 	//single_struct()
 	// array()
 	// embedded_struct()
-	custom_attr_name()
+	// custom_attr_name()
+	// simple_test()
+	timestamp_parse()
 }
