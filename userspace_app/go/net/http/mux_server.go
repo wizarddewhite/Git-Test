e51520d0ae8f4140e20ba61084fc0453917c0924
Wei Yang <richard.weiyang@gmail.com>
2021-06-23 15:07:20 +0800

    userspace/go: must use POST in curl
    Otherwise it will get 405 method not allowed.

--- userspace_app/go/net/http/mux_server.go
+++ userspace_app/go/net/http/mux_server.go
@@ -10,11 +10,18 @@ func YourHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Gorilla!\n"))
 }
 
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write([]byte("good"))
+}
+
 func handleFunc() {
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
 	r.HandleFunc("/", YourHandler)
-	r.HandleFunc("/user", YourHandler).Methods("GET")
+	r.HandleFunc("/users", YourHandler).Methods("GET")
+	r.HandleFunc("/user", handlePost).Methods("POST")
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8000", r))
@@ -24,11 +31,12 @@ func handler() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.Methods("GET").Path("/").Name("root").Handler(http.HandlerFunc(YourHandler))
 	router.Methods("GET").Path("/user").Name("root").Handler(http.HandlerFunc(YourHandler))
+	router.Methods("POST").Path("/token").Name("token").Handler(http.HandlerFunc(YourHandler))
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func main() {
-	//handleFunc()
+	// handleFunc()
 	handler()
 }
