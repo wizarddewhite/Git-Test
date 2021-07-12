f769cfc93448f1c0fcf98a93edc30a3cd926f2ff
Wei Yang <richard.weiyang@gmail.com>
2021-07-12 15:25:07 +0800

    userspace/bash: use function to refine example

--- userspace_app/bash/basic/for_loop.sh
+++ userspace_app/bash/basic/for_loop.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
 
-echo "from 0 to 10"
-for a in 0 1 2 3 4 5 6 7 8 9 10;
-do 
-	echo "  " $a;
-done
+function iterate_number()
+{
+	echo "from 0 to 10"
+	for a in 0 1 2 3 4 5 6 7 8 9 10;
+	do 
+		echo "  " $a;
+	done
 
-echo "echo file name"
-for file in `ls`;
-do
-	echo "  " $file;
-done
+	echo "from 10 to 19"
+	for ((i=10;i<20;i++));
+	do
+		echo "  " $i;
+	done
+}
 
-echo "from 10 to 19"
-for ((i=10;i<20;i++));
-do
-	echo "  " $i;
-done
+function list_files()
+{
+	echo "echo file name"
+	for file in `ls`;
+	do
+		echo "  " $file;
+	done
+}
 
-# iterate command line output
-local_files=`ls 2>/dev/null`
-IFS=$'\n'
-local_files=($local_files)
-for (( i=0; i<${#local_files[@]}; i++ ))
-do
-    echo ${local_files[$i]}
-done
+function iterate_output()
+{
+	# iterate command line output
+	local_files=`ls 2>/dev/null`
+	IFS=$'\n'
+	local_files=($local_files)
+	for (( i=0; i<${#local_files[@]}; i++ ))
+	do
+	    echo ${local_files[$i]}
+	done
+}
 
+# iterate_number
+# list_files
+iterate_output
