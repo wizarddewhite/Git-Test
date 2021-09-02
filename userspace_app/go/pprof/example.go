c4f8af78bea8922485e28cc41c49d10c7c5b71f9
Wei Yang <richard.weiyang@gmail.com>
2021-09-02 14:50:15 +0800

    userspace/go: more pprof material

--- userspace_app/go/pprof/example.go
+++ userspace_app/go/pprof/example.go
@@ -2,6 +2,24 @@
 // Usage:
 // 1. http://127.0.0.1:8080/debug/pprof/
 // 2. go tool pprof http://localhost:8080/debug/pprof/heap
+//      or
+//    curl -sK -v http://localhost:8080/debug/pprof/heap > heap.out
+//    go tool pprof heap.out
+// 3. go tool pprof heap.out
+//    (pprof) png
+//    (pprof) web
+
+// Official doc:
+//   https://golang.org/doc/diagnostics
+
+// MemoryLeak Example
+//   https://blog.detectify.com/2019/09/05/how-we-tracked-down-a-memory-leak-in-one-of-our-go-microservices/
+// Possible cause for memory leak
+//    Creating substrings and subslices.
+//    Wrong use of the defer statement.
+//    Unclosed HTTP response bodies (or unclosed resources in general).
+//    Orphaned hanging go routines.
+//    Global variables.
 
 package main
 
