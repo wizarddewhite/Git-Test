ca19ea1f345d3ab01f41f94b5dce6b33d9807e60
Wei Yang <richard.weiyang@gmail.com>
2021-09-10 14:58:00 +0800

    userspace/bash: some comment on getopts

--- userspace_app/bash/basic/getopts.sh
+++ userspace_app/bash/basic/getopts.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-while getopts ":vp:" opt; do
+path=""
+
+# getopts optstring name
+while getopts "vp:" opt; do
 	case "$opt" in
 	"v")
-		echo "get a"
+		echo "get option v"
 		;;
 	"p")
 		echo "p's parameter is: $OPTARG"
+		path=${OPTARG}
 		;;
 	":")
 		echo "no argument for option: $OPTARG"
 		;;
 	"?")
+		# this works when ":" is the first letter in optstring
 		echo "not valid option: $OPTARG"
 		;;
 	esac
 done
+
+if [ -z "${path}" ] ; then
+    echo path is a must
+fi
+
