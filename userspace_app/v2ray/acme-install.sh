1da25d5ecf8aae2477338e74c3afb5641c3e3515
Wei Yang <richard.weiyang@gmail.com>
2021-09-20 13:12:49 +0000

    userspace/v2ray: seems this is the installation step

--- userspace_app/v2ray/acme-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt install socat
-curl  https://get.acme.sh | sh
-source ~/.bashrc
-~/.acme.sh/acme.sh --issue -d weiyang.ga --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d weiyang.ga --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
--- userspace_app/v2ray/acme-renew.sh
+++ userspace_app/v2ray/acme-renew.sh
@@ -1,4 +1,5 @@
 # git clone https://github.com/acmesh-official/acme.sh.git
+# ./acme.sh --install -m shyw13@gmail.com
 # cd git/acme.sh
 ./acme.sh --issue -d freedomland.tk --standalone -k ec-256
 ./acme.sh --installcert -d freedomland.tk --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
