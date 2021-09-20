bb7be0225972406847dc281fea0f80764ed497df
Wei Yang <richard.weiyang@gmail.com>
2021-09-20 21:02:16 +0800

    userspace/v2ray: update acme cert

--- userspace_app/v2ray/acm-renew.sh
+++
@@ -1,4 +0,0 @@
-stop bee
-~/.acme.sh/acme.sh --issue -d freedomland.tk --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d freedomland.tk --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
-service nginx restart
---
+++ userspace_app/v2ray/acme-renew.sh
@@ -0,0 +1,6 @@
+# git clone https://github.com/acmesh-official/acme.sh.git
+# cd git/acme.sh
+./acme.sh --issue -d freedomland.tk --standalone -k ec-256
+./acme.sh --installcert -d freedomland.tk --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+stop bee
+service nginx restart
