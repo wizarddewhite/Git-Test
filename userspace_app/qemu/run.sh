14ae2f8f617bdad504ae3a0ba9f2bc024a4ca2b5
Wei Yang <richard.weiyang@gmail.com>
2022-03-23 07:49:05 +0800

    userspace/qemu: specify multi-numa vm

--- userspace_app/qemu/run.sh
+++ userspace_app/qemu/run.sh
@@ -4,3 +4,10 @@ sudo ~/git/qemu/build/qemu-system-x86_64 -m 4G,slots=32,maxmem=32G \
         -drive file=/home/richard/guest/project.img,format=qcow2 \
         -nographic
 
+sudo ~/git/qemu/build/qemu-system-x86_64 -m 6G,slots=32,maxmem=32G \
+        -smp 8 --enable-kvm \
+        -drive file=/home/richard/guest/fedora.img,format=raw \
+        -drive file=/home/richard/guest/project.img,format=qcow2 \
+        -nographic \
+        -object memory-backend-ram,id=mem1,size=3G -object memory-backend-ram,id=mem2,size=3G \
+        -numa node,nodeid=0,memdev=mem1 -numa node,nodeid=1,memdev=mem2
