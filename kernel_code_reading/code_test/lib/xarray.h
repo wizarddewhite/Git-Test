f26f251a368b83b7bfdd8fe4e0f9fd69116e561f
Wei Yang <richard.weiyang@gmail.com>
2022-05-27 10:35:24 +0800

    kernel_code_reading/code_test: let's explore xarray

---
+++ kernel_code_reading/code_test/lib/xarray.h
@@ -0,0 +1,43 @@
+#ifndef _XARRAY_H
+#define _XARRAY_H
+
+#include "bug.h"
+
+/*
+ * The bottom two bits of the entry determine how the XArray interprets
+ * the contents:
+ *
+ * 00: Pointer entry
+ * 10: Internal entry
+ * x1: Value entry or tagged pointer
+ *
+ * Attempting to store internal entries in the XArray is a bug.
+ *
+ * Most internal entries are pointers to the next node in the tree.
+ * The following internal entries have a special meaning:
+ *
+ * 0-62: Sibling entries
+ * 256: Retry entry
+ * 257: Zero entry
+ *
+ * Errors are also represented as internal entries, but use the negative
+ * space (-4094 to -2).  They're never stored in the slots array; only
+ * returned by the normal API.
+ */
+
+#define BITS_PER_XA_VALUE	(BITS_PER_LONG - 1)
+
+/**
+ * xa_mk_value() - Create an XArray entry from an integer.
+ * @v: Value to store in XArray.
+ *
+ * Context: Any context.
+ * Return: An entry suitable for storing in the XArray.
+ */
+static inline void *xa_mk_value(unsigned long v)
+{
+	WARN_ON((long)v < 0);
+	return (void *)((v << 1) | 1);
+}
+
+#endif /* _XARRAY_H */
---
+++ kernel_code_reading/code_test/xarray_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "xarray.h"
+#include "bug.h"
+
+int main()
+{
+	WARN(1, "just a test for WARN %s\n", "158");
+	return 0;
+}
