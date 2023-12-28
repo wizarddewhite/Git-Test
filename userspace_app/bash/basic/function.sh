9e9826178aa38bb917326aa26ad68628c7721ffd
Wei Yang <richard.weiyang@gmail.com>
2023-12-28 23:15:23 +0800

    userspace/bash: define local variable in function

--- userspace_app/bash/basic/function.sh
+++ userspace_app/bash/basic/function.sh
@@ -2,6 +2,8 @@
 
 function f1() 
 {
+	prune="ywtest f1"
+	local local_prune="ywtest local"
 	echo "this is function 1"
 }
 
@@ -13,6 +15,8 @@ function f2()
 	echo $# args: $*
 	echo '$0 is always the file name' $0
 	echo the first arg is \"$1\"
+	echo 'prune is' $prune
+	echo 'local_prune is' $local_prune
 	return 2
 }
 
