cb79b9702b8e2241a80e914b41ef9ef7f2e356d2
Wei Yang <richard.weiyang@gmail.com>
2023-12-28 23:09:19 +0800

    userspace/bash: fix typo

--- userspace_app/bash/basic/function.sh
+++ userspace_app/bash/basic/function.sh
@@ -2,14 +2,14 @@
 
 function f1() 
 {
-	echo "this is funciton 1"
+	echo "this is function 1"
 }
 
 f1
 
 function f2()
 {
-	echo "this is funciton 2"
+	echo "this is function 2"
 	echo $# args: $*
 	echo '$0 is always the file name' $0
 	echo the first arg is \"$1\"
