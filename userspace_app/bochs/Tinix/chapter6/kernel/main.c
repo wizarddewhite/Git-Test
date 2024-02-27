5552a53a4d37b579fdac34bc3f73064d41bb36ae
Wei Yang <richard.weiyang@gmail.com>
2024-02-27 17:22:29 +0800

    bochs/Tinix: chapter6 now enable timer interrupt
    in which do process scheduling.

--- userspace_app/bochs/Tinix/chapter6/kernel/main.c
+++ userspace_app/bochs/Tinix/chapter6/kernel/main.c
@@ -55,6 +55,9 @@ PUBLIC int tinix_main()
 
 	p_proc_ready	= proc_table;
 	
+	put_irq_handler(CLOCK_IRQ, clock_handler);	/* 设定时钟中断处理程序 */
+	enable_irq(CLOCK_IRQ);				/* 让8259A可以接收时钟中断 */
+
 	restart();
 
 
