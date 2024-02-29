eb3903f9ad7f5eee620cc249eb72e85025c965fb
Wei Yang <richard.weiyang@gmail.com>
2024-02-29 14:20:42 +0000

    bash/shlug: try to add some log

--- userspace_app/bash/shlug/shlug_cron.sh
+++ userspace_app/bash/shlug/shlug_cron.sh
@@ -3,9 +3,14 @@
 # Add this line in /etc/crontab
 #0 0     * * 2   root    /home/weiyang/go/shlug/shlug_cron.sh
 
+touch /root/shlug_notify
+date >> /root/shlug_notify
 cd /home/weiyang/go/shlug/script/
 export JUHE_APPKEY="your_key"
 ./check.sh
 result=$?
+echo "result is $?" >> /root/shlug_notify
 go run email.go $result
 echo y | /home/weiyang/go/shlug/script/shlug_ann_h4.sh
+echo "=========" >> /root/shlug_notify
+
