403f3d1c1d641f39ed66933c1d1fdf08b0062884
Wei Yang <richard.weiyang@gmail.com>
2024-02-29 13:09:57 +0000

    bash/shlug: crontab entry

--- userspace_app/bash/shlug/shlug_cron.sh
+++ userspace_app/bash/shlug/shlug_cron.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Add this line in /etc/crontab
+#0 0     * * 2   root    /home/weiyang/go/shlug/shlug_cron.sh
+
 cd /home/weiyang/go/shlug/script/
 export JUHE_APPKEY="your_key"
 ./check.sh
