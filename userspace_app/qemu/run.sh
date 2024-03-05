01d81e4faf3c3c286638a18cb484556860d67dbc
Wei Yang <richard.weiyang@gmail.com>
2024-03-05 10:48:52 +0800

    qemu: start kernel with single hello as initrd

--- userspace_app/qemu/run.sh
+++ userspace_app/qemu/run.sh
@@ -19,3 +19,11 @@ sudo ~/git/qemu/build/qemu-system-x86_64 -m 4G,slots=32,maxmem=32G \
         -drive file=/home/richard/guest/project.img,format=qcow2 \
         -vnc :0
 
+# start raw kernel with customized initrd
+# where rootfs is built from hello.c. Read hello.c for usage
+# in qemu monitor, use "change vnc password"
+sudo /home/richard/git/qemu/build/qemu-system-x86_64  \
+  -m 4G -smp 8 --enable-kvm \
+  -kernel /home/richard/git/linux/arch/x86/boot/bzImage \
+  -initrd ./rootfs -append "root=/dev/ram rdinit=/hello" \
+  -vnc :0,password -monitor stdio
