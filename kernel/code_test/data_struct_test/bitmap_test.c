11c5a4fe61360978bb1d8195507eb6669b3df48b
Wei Yang <richard.weiyang@gmail.com>
2024-04-10 11:10:58 +0800

    data_struct_test/bitmap: fill and clear

--- kernel/code_test/data_struct_test/bitmap_test.c
+++ kernel/code_test/data_struct_test/bitmap_test.c
@@ -4,7 +4,8 @@
 #include "bitops.h"
 #include "test_common.h"
 
-DECLARE_BITMAP(bitmaps, 35);
+#define TESTS_BITS 35
+DECLARE_BITMAP(bitmaps, TESTS_BITS);
 
 void bitmap_zero_checks()
 {
@@ -16,10 +17,53 @@ void bitmap_zero_checks()
 
 	PREFIX_PUSH();
 
-	bitmap_zero(bitmaps, 35);
+	bitmap_zero(bitmaps, TESTS_BITS);
 
-	for(i = 0; i < 35; i++)
+	for(i = 0; i < TESTS_BITS; i++)
 		ASSERT_FALSE(test_bit(i, bitmaps));
+
+	test_pass_pop();
+
+	prefix_pop();
+}
+
+void bitmap_fill_checks()
+{
+	int i;
+
+	prefix_reset();
+	prefix_push("bitmap_fill");
+	test_print("Running %s tests...\n", "bitmap_fill");
+
+	PREFIX_PUSH();
+
+	bitmap_fill(bitmaps, TESTS_BITS);
+
+	for(i = 0; i < TESTS_BITS; i++)
+		ASSERT_TRUE(test_bit(i, bitmaps));
+
+	test_pass_pop();
+
+	prefix_pop();
+}
+
+void bitmap_clear_checks()
+{
+	int i;
+
+	prefix_reset();
+	prefix_push("bitmap_clear");
+	test_print("Running %s tests...\n", "bitmap_clear");
+
+	PREFIX_PUSH();
+
+	bitmap_fill(bitmaps, TESTS_BITS);
+
+	for(i = 0; i < TESTS_BITS; i++) {
+		ASSERT_TRUE(test_bit(i, bitmaps));
+		bitmap_clear(bitmaps, i, 1);
+		ASSERT_FALSE(test_bit(i, bitmaps));
+	}
 
 	test_pass_pop();
 
@@ -31,5 +75,7 @@ int main(int argc, char *argv[])
 	parse_args(argc, argv);
 
 	bitmap_zero_checks();
+	bitmap_fill_checks();
+	bitmap_clear_checks();
 	return 0;
 }
