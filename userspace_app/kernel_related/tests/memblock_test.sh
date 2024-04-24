ee1933ddf9b310105c3a3bf1f2634a07038218f9
Wei Yang <richard.weiyang@gmail.com>
2024-04-24 10:38:24 +0800

    kernel_related/tests: test NUMA case

--- userspace_app/kernel_related/tests/memblock_test.sh
+++ userspace_app/kernel_related/tests/memblock_test.sh
@@ -5,8 +5,12 @@ if [[ $? != 0 ]]; then
 	echo Error: Not in kernel dir?
 	exit
 fi
-make
 echo === Run memblock test
+make > /dev/null
 ./main
 echo === memblock test finish
+echo === Run memblock test with NUMA
+make NUMA=1 > /dev/null
+./main
+echo === memblock test with NUMA finish
 popd
