2afbe7226530e6c6fe8eaf24e3cd89a0eb27ed13
Wei Yang <richard.weiyang@gmail.com>
2024-04-28 06:37:29 +0800

    c/linker_script: an example for asm

---
+++ userspace_app/c/linker_script/hello.asm
@@ -0,0 +1,19 @@
+
+	global _start
+
+	section .text
+_start:
+	; write(1, message, 13)
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, message
+	mov	rdx, 13
+	syscall
+
+	; exit(0)
+	mov	eax, 60
+	xor	rdi, rdi
+	syscall
+
+message:
+	db	"Hello, World", 10
