697da569f1341e9715064a9e5e511760014abe4e
Wei Yang <richard.weiyang@gmail.com>
2024-04-28 06:25:54 +0800

    c/linker_script: split a function

--- userspace_app/c/linker_script/main.c
+++ userspace_app/c/linker_script/main.c
@@ -2,12 +2,16 @@
 int global_data = 4;
 int global_data_2;
 
+void global_data_default()
+{
+	printf("global_data = %d\n", global_data);
+	printf("global_data_2 = %d\n", global_data_2);
+}
+
 int main(int argc, char **argv)
 {
 	int local_data = 3;
-	printf("Hello Worldn");
-	printf("global_data = %d\n", global_data);
-	printf("global_data_2 = %d\n", global_data_2);
+	global_data_default();
 	printf("local_data = %d\n", local_data);
 	return (0);
 }
