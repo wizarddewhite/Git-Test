7dd34d08d525335fa0b55854c3b6362ca15de0e7
Wei Yang <richard.weiyang@gmail.com>
2024-04-28 07:41:08 +0800

    c/linker_script: use strlen to get message length

--- userspace_app/c/linker_script/hello.asm
+++ userspace_app/c/linker_script/hello.asm
@@ -3,11 +3,15 @@
 
 	section .text
 _start:
+	; strlen(message)
+	mov	rdi, message
+	call	strlen
+	mov	rdx, rax
+
 	; write(1, message, 13)
 	mov	rax, 1
 	mov	rdi, 1
 	mov	rsi, message
-	mov	rdx, 13
 	syscall
 
 	; exit(0)
@@ -15,5 +19,25 @@ _start:
 	xor	rdi, rdi
 	syscall
 
+; ------------------------------------------------------------------------
+; int strlen(char* p_str);
+; ------------------------------------------------------------------------
+strlen:
+	mov	rax, 0			; 字符串长度开始是 0
+	mov	rsi, rdi		; rdi 是函数参数,rsi 指向首地址
+
+.1:
+	cmp	byte [rsi], 0		; 看 rsi 指向的字符是否是 '\0'
+	jz	.2			; 如果是 '\0'，程序结束
+	inc	rsi			; 如果不是 '\0'，rsi 指向下一个字符
+	inc	rax			;	  并且，eax 自加一
+	jmp	.1			; 如此循环
+
+.2:
+	ret				; 函数结束，返回
+; ------------------------------------------------------------------------
+
+
+	section .data
 message:
 	db	"Hello, World", 10
