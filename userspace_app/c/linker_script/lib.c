eb090e9ddc8a36cc346487869a33a2b40a65e973
Wei Yang <richard.weiyang@gmail.com>
2024-04-28 10:53:50 +0800

    c/linker_script: define __init_begin in lds and print it

--- userspace_app/c/linker_script/lib.c
+++ userspace_app/c/linker_script/lib.c
@@ -5,4 +5,55 @@ extern int print(char *msg);
 void __section(".meminit.text") test_func()
 {
 	print("A test function\n");
+
+	show_init_begin_end();
+}
+
+void show_init_begin_end()
+{
+	char buf[128];
+
+	print("__init_begin: ");
+	itoa(buf, (unsigned long)&__init_begin);
+	print(buf);
+	print("\n");
+
+	print("__init_end:   ");
+	itoa(buf, (unsigned long)&__init_end);
+	print(buf);
+	print("\n");
+
+}
+
+/* 数字前面的 0 不被显示出来, 比如 0000B800 被显示成 B800 */
+char * itoa(char * str, unsigned long num)
+{
+	char *	p = str;
+	char	ch;
+	int	i;
+	int	flag = FALSE;
+
+	*p++ = '0';
+	*p++ = 'x';
+
+	if(num == 0){
+		*p++ = '0';
+	}
+	else{	
+		for(i=60;i>=0;i-=4){
+			ch = (num >> i) & 0xF;
+			if(flag || (ch > 0)){
+				flag = TRUE;
+				ch += '0';
+				if(ch > '9'){
+					ch += 7;
+				}
+				*p++ = ch;
+			}
+		}
+	}
+
+	*p = 0;
+
+	return str;
 }
--- userspace_app/c/linker_script/lib.h
+++ userspace_app/c/linker_script/lib.h
@@ -3,4 +3,11 @@
 
 #define __section(section)              __attribute__((__section__(section)))
 
+#define TRUE  1
+#define FALSE 0
+
+extern char __init_begin[], __init_end[];
+char * itoa(char * str, unsigned long num);
+void show_init_begin_end();
+
 #endif //  __LIB_H_
