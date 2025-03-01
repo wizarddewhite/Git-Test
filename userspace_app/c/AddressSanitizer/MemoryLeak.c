917beb6b9c7e2044d63a11b5cdfa717ff31135f5
Wei Yang <richard.weiyang@gmail.com>
2025-03-01 15:42:48 +0800

    userspace/c: sanitizer couldn't detect memory leak of global variable

--- userspace_app/c/AddressSanitizer/MemoryLeak.c
+++ userspace_app/c/AddressSanitizer/MemoryLeak.c
@@ -7,12 +7,17 @@ char* getMemory()
 	return p;
 }
 
+static void *mem;
+
 int main()
 {
 	char *p = getMemory();
 	// Fix memory leak by free it before exit
 	// free(p);
 	p = NULL;
+
+	// but allocate to a global memory will not detect memory leak
+	mem = malloc(10);
 
 	return 0;
 }
