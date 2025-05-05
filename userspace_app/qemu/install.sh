5ae0b3ff470f074f07f844c5a217a7be408428d6
Wei Yang <richard.weiyang@gmail.com>
2025-05-05 10:47:59 +0800

    qemu/install: install guest with vnc support

--- userspace_app/qemu/install.sh
+++ userspace_app/qemu/install.sh
@@ -8,6 +8,11 @@ sudo ~/git/qemu/x86_64-softmmu/qemu-system-x86_64 -m 8G -smp 8 --enable-kvm \
 
 
 # normal install with vnc
+# 如果是password,则需要在启动后，要在monitor中运行"change vnc password"来修改vnc密码才能成功连接。
+# 如果是password=off，则不需要在monitor中设置密码了。
+# 在mac上，直接CMD+Space，输入vnc://台式机的ip。台式机本身没有开vnc，默认虚拟机的vnc端口还是5900。
 sudo ~/git/qemu/x86_64-softmmu/qemu-system-x86_64 -m 8G -smp 8 --enable-kvm \
+-vnc :0,password=off -monitor stdio \
 -drive file=/home/richard/guest/fedora29.img.back \
--drive file=/home/richard/guest/isos/Fedora-Server-dvd-x86_64-29-1.2.iso,media=cdrom 
+-drive file=/home/richard/guest/isos/Fedora-Server-dvd-x86_64-29-1.2.iso,media=cdrom \
+-boot d
