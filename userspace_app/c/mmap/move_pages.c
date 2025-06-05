1f3efd71256cfe383f9eac713bd6e573f8381aad
Wei Yang <richard.weiyang@gmail.com>
2025-06-05 16:07:50 +0800

    c/mmap: copy migrate test
    This test code is copied from kernel commit:
    
    commit a7504ed14f9b5e873599b2487eb95062dd0b65f8
    Author: Ying Huang <huang.ying.caritas@gmail.com>
    Date:   Wed Aug 17 16:14:01 2022 +0800
    
        migrate: fix syscall move_pages() return value for failure
    
    The error-injection/test code could be found in this mail.
    
    https://lkml.kernel.org/r/20220817081408.513338-1-ying.huang@intel.com

--- userspace_app/c/mmap/move_pages.c
+++ userspace_app/c/mmap/move_pages.c
@@ -1,12 +1,17 @@
 #define _GNU_SOURCE
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+
 #include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <numaif.h>
 #include <numa.h>
+#include <fcntl.h>
+#include <sys/uio.h>
 
 int move_to_different_node()
 {
@@ -76,9 +81,146 @@ int move_to_different_node()
 	return 0;
 }
 
+#ifndef MADV_FREE
+#define MADV_FREE      8               /* free pages only if memory pressure */
+#endif
+
+#define ONE_MB         (1024 * 1024)
+#define MAP_SIZE       (16 * ONE_MB)
+#define THP_SIZE       (2 * ONE_MB)
+#define THP_MASK       (THP_SIZE - 1)
+
+#define ERR_EXIT_ON(cond, msg)                                 \
+        do {                                                   \
+                int __cond_in_macro = (cond);                  \
+                if (__cond_in_macro)                           \
+                        error_exit(__cond_in_macro, (msg));    \
+        } while (0)
+
+void error_msg(int ret, int nr, int *status, const char *msg)
+{
+        int i;
+
+        fprintf(stderr, "Error: %s, ret : %d, error: %s\n",
+                msg, ret, strerror(errno));
+
+        if (!nr)
+                return;
+        fprintf(stderr, "status: ");
+        for (i = 0; i < nr; i++)
+                fprintf(stderr, "%d ", status[i]);
+        fprintf(stderr, "\n");
+}
+
+void error_exit(int ret, const char *msg)
+{
+        error_msg(ret, 0, NULL, msg);
+        exit(1);
+}
+
+int page_size;
+
+bool do_vmsplice;
+bool do_thp;
+
+static int pipe_fds[2];
+void *addr;
+char *pn;
+char *pn1;
+void *pages[2];
+int status[2];
+
+void prepare()
+{
+        int ret;
+        struct iovec iov;
+
+        if (addr) {
+                munmap(addr, MAP_SIZE);
+                close(pipe_fds[0]);
+                close(pipe_fds[1]);
+        }
+
+        ret = pipe(pipe_fds);
+        ERR_EXIT_ON(ret, "pipe");
+
+        addr = mmap(NULL, MAP_SIZE, PROT_READ | PROT_WRITE,
+                    MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        ERR_EXIT_ON(addr == MAP_FAILED, "mmap");
+        if (do_thp) {
+                ret = madvise(addr, MAP_SIZE, MADV_HUGEPAGE);
+                ERR_EXIT_ON(ret, "advise hugepage");
+        }
+
+        pn = (char *)(((unsigned long)addr + THP_SIZE) & ~THP_MASK);
+        pn1 = pn + THP_SIZE;
+        pages[0] = pn;
+        pages[1] = pn1;
+        *pn = 1;
+
+        if (do_vmsplice) {
+                iov.iov_base = pn;
+                iov.iov_len = page_size;
+                ret = vmsplice(pipe_fds[1], &iov, 1, 0);
+                ERR_EXIT_ON(ret < 0, "vmsplice");
+        }
+
+        status[0] = status[1] = 1024;
+}
+
+void test_migrate()
+{
+        int ret;
+        int nodes[2] = { 1, 1 };
+        pid_t pid = getpid();
+
+        prepare();
+        ret = move_pages(pid, 1, pages, nodes, status, MPOL_MF_MOVE_ALL);
+        error_msg(ret, 1, status, "move 1 page");
+
+        prepare();
+        ret = move_pages(pid, 2, pages, nodes, status, MPOL_MF_MOVE_ALL);
+        error_msg(ret, 2, status, "move 2 pages, page 1 not mapped");
+
+        prepare();
+        *pn1 = 1;
+        ret = move_pages(pid, 2, pages, nodes, status, MPOL_MF_MOVE_ALL);
+        error_msg(ret, 2, status, "move 2 pages");
+
+        prepare();
+        *pn1 = 1;
+        nodes[1] = 0;
+        ret = move_pages(pid, 2, pages, nodes, status, MPOL_MF_MOVE_ALL);
+        error_msg(ret, 2, status, "move 2 pages, page 1 to node 0");
+}
+
+int move_and_check_status()
+{
+        numa_run_on_node(0);
+        page_size = getpagesize();
+
+        test_migrate();
+
+        fprintf(stderr, "\nMake page 0 cannot be migrated:\n");
+        do_vmsplice = true;
+        test_migrate();
+
+        fprintf(stderr, "\nTest THP:\n");
+        do_thp = true;
+        do_vmsplice = false;
+        test_migrate();
+
+        fprintf(stderr, "\nTHP: make page 0 cannot be migrated:\n");
+        do_vmsplice = true;
+        test_migrate();
+
+        return 0;
+}
+
 int main(void)
 {
 	move_to_different_node();
+	move_and_check_status();
 
 	return 0;
 }
